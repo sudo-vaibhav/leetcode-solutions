4358912323d3c7dcece5a48f94a73539ef8f3b22
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-20 00:08:27 +0530

    Time: 9 ms (5.27%), Space: 6.3 MB (35.34%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int populateNumVec(string version, vector<int>& num){
+        int temp = 0;
+        for(auto i:version){
+            if(isdigit(i)){
+                temp=temp*10+(i-'0');
+            }
+            else{
+                num.push_back(temp);
+                temp = 0;
+            }
+        }
+        
+        num.push_back(temp);
+        return num.size();
+    }
+    int compareVersion(string version1, string version2) {
+        vector<int> num1,num2;
+        int N = populateNumVec(version1,num1);
+        int M = populateNumVec(version2,num2);
+        
+        int i = 0;
+        while(i<max(N,M)){
+            auto v1 = 0;
+            auto v2 = 0;
+            
+            if(i<N){
+                v1 = num1[i];
+            }
+            if(i<M){
+                v2 = num2[i];
+            }
+            
+            cout<<v1<<" "<<v2<<"\n";
+            if(v1>v2){
+                return 1;
+            }
+            else if(v2>v1){
+                return -1;
+            }
+            
+            i++;
+        }
+        
+        return 0;
+    }
+};

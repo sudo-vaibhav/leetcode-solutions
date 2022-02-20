b5be98d902c039aed38ab6ea41f5211e2e578f16
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-20 15:30:33 +0530

    Time: 330 ms (5.02%), Space: 48.1 MB (5.04%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        // sort on basis of start of intervals
         sort(intervals.begin(),intervals.end(),[](vector<int> a,vector<int> b){
             return a[0]<b[0];
         });
         
         vector<vector<int>> ans;
+        
+        // seed the prev with first value of newly sorted intervals
         vector<int> prev = intervals[0];
         
         for(auto i=1;i<intervals.size();i++){
@@ -17,13 +21,22 @@ public:
                 prev = cur;
             }
             else{
+                // overlap exists
+                
+                // pick best possible lower value
                 prev[0] = min(prev[0],cur[0]);
+                
+                // pick best possible upper value
                 prev[1] = max(prev[1],cur[1]);
             }
         }
         
+        
+        // there will always be one remainder ans left in prev, add that also
         ans.push_back(prev);
         
+        
+        // finally return answer
         return ans;
         
     }

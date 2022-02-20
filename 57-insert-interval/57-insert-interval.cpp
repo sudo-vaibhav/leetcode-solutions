7411fe29f71cf731b027577ea638f350ae7b3c4d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-20 16:32:36 +0530

    Time: 16 ms (80.75%), Space: 17.5 MB (18.20%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -41,42 +41,28 @@ public:
         
         vector<int> goalInterval = newInterval;
         vector<vector<int>> ans;
-        
+        bool done = false;
         for(auto i:intervals){
             // if no overlap
-            if(i[0]>newInterval[1]||newInterval[0]>i[1]){
+            if(newInterval[0]>i[1]){
                 // no need for adjusting goal
                 // also because there is no overlap, we can push this straight to
                 // answer
                 ans.push_back(i);
             }
-            else{
+            else if(i[0]>newInterval[1]){
+                if(!done){
+                    done = true;
+                    ans.push_back(goalInterval);
+                }
+                ans.push_back(i);
+            }
+            else {
                 goalInterval[0] = min(i[0],goalInterval[0]);
                 goalInterval[1] = max(i[1],goalInterval[1]);
             }
         }
-        
-        // finally insert goal interval to ans
-        ans.push_back(goalInterval);
-        
-        // find first time where goalInterval[0] is exceeded
-        int exceed_pos = 0;
-        for(exceed_pos=0;exceed_pos<ans.size();++exceed_pos){
-            auto cur = ans[exceed_pos];
-            // cout<<"cur: "<<cur[0]<<endl;
-            if(cur[0]>=goalInterval[0]){
-                break;
-            }
-        }
-        
-        // now starting from end to exceed pos , shift all elements to right
-        for(int i = ans.size()-1;i>exceed_pos;--i){
-            ans[i] = ans[i-1];
-        }
-        
-        // finally put goal interval in its place
-        ans[exceed_pos] = goalInterval;
-        
+        if(!done) ans.push_back(goalInterval);
         return ans;
     }
 };

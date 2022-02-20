5ac15a8cc61813ca6ab4a9e7901809e32a3919e7
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-20 22:06:29 +0530

    Time: 13 ms (70.68%), Space: 8.3 MB (20.39%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -4,36 +4,54 @@ public:
     vector<int> z;
     
     void populateZArray(string s){
+        // initialise l, r and n
         int l = 0,r=0,n = s.size();
+        
+        // initialise z vector
         z = vector<int>(n);
+        
+        // from index 1 to n-1 , find length of prefix match found at position i
         for(int i=1;i<s.size();i++){
+            
+            // take boost if you are in the l to r zone
             if(i<=r){
                 z[i] = min(r-i+1,z[i-l]);
             }
-            
+            // increment z[i] if current matches happen
             while(i+z[i]<n&&s[z[i]]==s[i+z[i]]){
                 z[i]++;
             }
             
+            // reconfigure l and r if you have exceeded current boundary
             if(i+z[i]-1>r){
                 r = i+z[i]-1;
                 l = i;
             }
+            
         }
+        
     }
     
     int strStr(string haystack, string needle) {
-        string temp = needle+"#"+haystack;
-        populateZArray(temp);
         if(needle=="") return 0;
-        cout<<temp<<endl;
         
+        // add a delimiter between needle and haystack
+        // this delimiter should not be part of character space of 
+        // given input
+        string temp = needle+"#"+haystack;
+        
+        // call z-function
+        populateZArray(temp);
+        
+        // inside haystack find those places whose z value 
+        // is equal to length of needle
         for(int i=needle.size()+1;i<temp.size();++i){
             if(z[i]==needle.size()){
                 return i-needle.size()-1;    
             }
         }
         
+        // if no match return -1
         return -1;
     }
 };

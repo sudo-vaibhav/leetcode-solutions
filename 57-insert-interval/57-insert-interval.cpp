d895fba190767e26f6c8b0012e5f9d75622a9e63
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-20 16:17:01 +0530

    Time: 28 ms (37.66%), Space: 17.7 MB (13.43%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,3 +1,40 @@
+// O(nlogn) solution as we are still sorting in end, which can be avoided
+// class Solution {
+// public:
+//     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+//         vector<int> goalInterval = newInterval;
+//         vector<vector<int>> ans;
+        
+//         for(auto i:intervals){
+//             // if no overlap
+//             if(i[0]>newInterval[1]||newInterval[0]>i[1]){
+//                 // no need for adjusting goal
+//                 // also because there is no overlap, we can push this straight to
+//                 // answer
+//                 ans.push_back(i);
+//             }
+//             else{
+//                 goalInterval[0] = min(i[0],goalInterval[0]);
+//                 goalInterval[1] = max(i[1],goalInterval[1]);
+//             }
+//         }
+        
+//         // finally insert goal interval to ans
+//         ans.push_back(goalInterval);
+        
+//         // sort array again so that goal interval goes in its place
+        
+//         sort(ans.begin(),ans.end(),[](vector<int> a, vector<int> b){
+//             return a[0]<b[0];
+//         });
+     
+//         return ans;
+//     }
+// };
+
+
+// O(n) approach, without sorting
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
@@ -22,12 +59,26 @@ public:
         // finally insert goal interval to ans
         ans.push_back(goalInterval);
         
-        // sort array again so that goal interval goes in its place
+        // find first time where goalInterval[0] is exceeded
+        int exceed_pos = 0;
+        for(exceed_pos=0;exceed_pos<ans.size();++exceed_pos){
+            auto cur = ans[exceed_pos];
+            // cout<<"cur: "<<cur[0]<<endl;
+            if(cur[0]>=goalInterval[0]){
+                break;
+            }
+        }
         
-        sort(ans.begin(),ans.end(),[](vector<int> a, vector<int> b){
-            return a[0]<b[0];
-        });
-     
+        // now starting from exceed pos to end-1 , shift all elements to right
+        // if(ans.size())
+        // cout<<"size:"<<ans.size()<<endl;
+        // cout<<"exceed pos:"<<exceed_pos<<endl;
+        for(int i = ans.size()-1;i>exceed_pos;--i){
+            ans[i] = ans[i-1];
+        }
+//         // finally put goal interval in its place
+        ans[exceed_pos] = goalInterval;
+        
         return ans;
     }
 };

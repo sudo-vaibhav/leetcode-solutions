99e73e679aff5a9ce276832d07f36f2ad444b5df
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-21 13:01:15 +0530

    Time: 29 ms (44.20%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> occurenceCount;
-        int n = nums.size();
-        for(auto num: nums){
-            occurenceCount[num]++;
-        }
-        
-        int maxNum,maxCount=0;
-        
-        for(auto i:occurenceCount){
-            if(i.second>maxCount){
-                maxCount = i.second;
-                maxNum = i.first;
+        int candidate,count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                candidate = nums[i];
             }
+            if(candidate == nums[i]) count++;
+            else count--;
         }
         
-        return maxNum;
+        return candidate;
     }
 };

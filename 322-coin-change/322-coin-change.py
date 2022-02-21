02750b80e9b3417e215ae82382c1a84d0a6a7d9f
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-21 17:07:39 +0530

    Time: 7567 ms (5.00%), Space: 509.6 MB (5.14%) - LeetHub

---
+++ 322-coin-change/322-coin-change.py
@@ -0,0 +1,25 @@
+class Solution:
+    @cache
+    def solve(self,n,amount):
+        if amount == 0 : return 0
+        if amount<0 : return -1
+        if(n==0): return -1 
+        
+        coinVal = self.coins[n-1] # 5
+        
+        res1 = self.solve(n,amount-coinVal) # amount = -2
+        res2 = self.solve(n-1,amount) # n = 2
+        
+        if(res1==-1):
+            return res2
+
+        
+        if(res2 == -1):
+            return 1+res1
+        return min(1+res1,res2)
+        
+        
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        self.coins = coins # [1,2,5]
+        n = len(coins) # 3
+        return self.solve(n,amount)

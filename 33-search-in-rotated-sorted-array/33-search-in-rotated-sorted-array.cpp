f402df597e193aa8064dca7499a49a15218072f2
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-22 01:01:12 +0530

    Time: 7 ms (41.86%), Space: 11.1 MB (29.77%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
-    int target;
-    int solve(vector<int>& nums,int L, int R){
-        int mid = L+(R-L)/2; // 3 
+    int solve(vector<int>& nums,int& target, int L, int R){
+        int mid = L+(R-L)/2;
         int val = nums[mid];
         if(val==target) return mid;
         if(L>=R) return -1;
-        return max(solve(nums, mid+1,R),solve(nums, L, mid-1));  
+        return max(solve(nums,target, mid+1,R),solve(nums,target, L, mid-1));  
     }
-    int search(vector<int>& nums, int t) {
-        // target 0
-        // 2 ,4,5,6,7,    0,1,
-        // L      M         R
-        target = t;
-        int L = 0;
-        int R = nums.size()-1; // 6
-        
-        return solve(nums, L,R);
-        
+    int search(vector<int>& nums, int target) {
+        return solve(nums, target, 0, nums.size()-1);
     }
 };

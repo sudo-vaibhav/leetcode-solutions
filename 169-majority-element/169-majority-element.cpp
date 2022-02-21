ef372a27336f0ce7f5f8cb7f8d5ea2d18e7e9cbc
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-21 13:08:25 +0530

    Time: 27 ms (50.68%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate,count=0;
+        int candidate;
+        unsigned short count=0;
         for(int i=0;i<nums.size();i++){
             if(count==0){
                 candidate = nums[i];

091f03995e1e90081e64a95047a5eb77d6a54f32
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-23 15:35:27 +0530

    Time: 96 ms (95.10%), Space: 61.3 MB (60.84%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,41 +1,53 @@
 
-int Partition(vector<int> &v, int start, int end){
+// int Partition(vector<int> &v, int start, int end){
 	
-	int pivot = end;
-	int j = start;
-	for(int i=start;i<end;++i){
-		if(v[i]<v[pivot]){
-			swap(v[i],v[j]);
-			++j;
-		}
-	}
-	swap(v[j],v[pivot]);
-	return j;
+// 	int pivot = end;
+// 	int j = start;
+// 	for(int i=start;i<end;++i){
+// 		if(v[i]<v[pivot]){
+// 			swap(v[i],v[j]);
+// 			++j;
+// 		}
+// 	}
+// 	swap(v[j],v[pivot]);
+// 	return j;
 	
-}
+// }
 
-void Quicksort(vector<int> &v, int start, int end ){
+// void Quicksort(vector<int> &v, int start, int end ){
 
-	if(start<end){
-		int p = Partition(v,start,end);
-		Quicksort(v,start,p-1);
-		Quicksort(v,p+1,end);
-	}
+// 	if(start<end){
+// 		int p = Partition(v,start,end);
+// 		Quicksort(v,start,p-1);
+// 		Quicksort(v,p+1,end);
+// 	}
 	
-}
+// }
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         auto n = nums.size();
+//         Quicksort(nums,0,n-1);
+//         cout<<endl;
+//         for(auto i=0;i<n-1;i++){
+//            if(nums[i]==nums[i+1])
+//            {
+//                return nums[i];
+//            }        
+//         }
+//         return 0;
+//     }
+// };
 
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        auto n = nums.size();
-        Quicksort(nums,0,n-1);
-        cout<<endl;
-        for(auto i=0;i<n-1;i++){
-           if(nums[i]==nums[i+1])
-           {
-               return nums[i];
-           }        
-        }
+        for(int i=0;i<nums.size();i++){
+            auto val = abs(nums[i]);
+            if(nums[val-1]<0) return val;
+            nums[val-1] = -nums[val-1];
+        }       
         return 0;
     }
 };

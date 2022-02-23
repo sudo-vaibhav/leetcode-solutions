05fd580e6103e1220c752db7e7b6f8174a865734
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-23 18:00:01 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (73.95%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,44 +1,62 @@
-class Solution {
-public:
-    void solve(vector<int>& nums, vector<multiset<int>>&ans){
+// APPROACH 1 using multiset
+// class Solution {
+// public:
+//     void solve(vector<int>& nums, vector<multiset<int>>&ans){
+//         int n = nums.size();
+//         for(int i=0;i<pow(2,n);i++){
+//             auto temp = i;
+//             multiset<int> curSubset;
+//             int j=0;
+//             while(temp){
+//                 auto toTake = temp&1;
+//                 temp>>=1;
+//                 if(toTake) curSubset.insert(nums[j]);      
+//                 j++;
+//             }
+//             bool dup = false;
+//             for(auto i:ans){
+//                 if (curSubset==i) dup=true;
+//             }
+//             if (!dup){
+//                 ans.push_back(curSubset);
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         vector<multiset<int>> ans;
+//         solve(nums,ans);
+//         vector<vector<int>> res;
+//         for(auto i:ans){
+//             res.push_back(vector<int>(i.begin(),i.end()));
+//         }
+//         return res;
+//     }
+// };
+
+
+// APPROACH 2
+class Solution{
+    void solve(int idx, vector<int>& nums,vector<int>& ans,vector<vector<int>>& res){
+        // first insert whatever is already there, coz its a valid subset
+        res.push_back(ans);
         int n = nums.size();
-        // if(idx==n){
-        
-        // }
-        // else{
-        //     for(int i=idx;i<n;i++){
-        //         swap(nums[i],nums[idx]);
-        //         solve(i+1,nums,ans);
-        //         swap(nums[i],nums[idx]);
-        //     }
-        // }
-        for(int i=0;i<pow(2,n);i++){
-            auto temp = i;
-            multiset<int> curSubset;
-            int j=0;
-            while(temp){
-                auto toTake = temp&1;
-                temp>>=1;
-                if(toTake) curSubset.insert(nums[j]);      
-                j++;
-            }
-            bool dup = false;
-            for(auto i:ans){
-                if (curSubset==i) dup=true;
-            }
-            if (!dup){
-                ans.push_back(curSubset);
+        if(idx==n) return;
+        for(int i=idx;i<n;i++){
+            if(i!=idx&&nums[i]==nums[i-1]) continue;
+            else{
+                ans.push_back(nums[i]);
+                solve(i+1,nums,ans,res);
+                ans.pop_back();
             }
         }
     }
-    
+    public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<multiset<int>> ans;
-        solve(nums,ans);
+        sort(nums.begin(),nums.end());
+        vector<int> ans;
         vector<vector<int>> res;
-        for(auto i:ans){
-            res.push_back(vector<int>(i.begin(),i.end()));
-        }
+        solve(0,nums,ans,res);
         return res;
     }
 };

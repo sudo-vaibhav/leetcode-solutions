3a1b7dc22a07b0f3f43d0d8e33157c09a0dd0b23
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-24 18:27:46 +0530

    Time: 20 ms (34.16%), Space: 9.6 MB (29.87%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,54 +1,45 @@
 class Solution {
 public:
-    vector<string> convert(vector<vector<int>>& matrix){
+    unordered_map<int,bool> rowMap, upperDiagMap, lowerDiagMap;
+    vector<string> convert(vector<vector<bool>>& matrix){
         vector<string> res;
         for(auto row:matrix){
             string temp = "";
             for(auto v:row){
-                temp+= (v==0) ? "." : "Q";
+                temp+= (v==false) ? "." : "Q";
             }
             
             res.push_back(temp);
         }
         return res;
     }
-    bool check(int row,int col,int n,vector<vector<int>>& matrix){
-        // for(int j=0;j<n;j++){
-        //     if(matrix[j][col]) return false;
-        // }
-        for(int j=0;j<n;j++){
-            if(matrix[row][j]) return false;
-        }
-        int i=0;
-        for(int j=col-1;j>=0;j--){
-            i++;
-            if(row-i>=0 && matrix[row-i][j]){
-                return false;
-            }
-            if(row+i<n && matrix[row+i][j]){
-                return false;
-            }
-        }
-        
+    bool check(int row,int col){
+        if(rowMap.count(row)||upperDiagMap.count(col-row)||lowerDiagMap.count(col+row)) return false;
         return true;
     }
-    void solve(int col, int n, vector<vector<int>>& matrix,vector<vector<string>>& ans){
+    void solve(int col, int n, vector<vector<bool>>& matrix,vector<vector<string>>& ans){
         for(int row = 0;row<n;row++){
-            if(check(row,col,n,matrix)){
+            if(check(row,col)){
                 matrix[row][col] = 1;
+                rowMap[row]=true; 
+                upperDiagMap[col-row]=true;
+                lowerDiagMap[col+row]=true;
                 if(col==n-1){
                     ans.push_back(convert(matrix));
                 }else{
                     solve(col+1,n,matrix,ans);    
                 }
+                rowMap.erase(row); 
+                upperDiagMap.erase(col-row); 
+                lowerDiagMap.erase(col+row);
                 matrix[row][col] = 0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<int>> queenMatrix;
+        vector<vector<bool>> queenMatrix;
         for(int i=0;i<n;i++){
-            queenMatrix.push_back(vector<int>(n,0));
+            queenMatrix.push_back(vector<bool>(n,false));
         }
         vector<vector<string>> ans;
         solve(0,n,queenMatrix,ans);

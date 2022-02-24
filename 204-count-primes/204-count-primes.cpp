c23cb54a462b5851190ec733752683ffa6f19598
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-24 15:33:14 +0530

    Time: 567 ms (15.74%), Space: 130.6 MB (7.10%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2){
+            return 0;
+        }
+        vector<int> cache(n,1);
+        cache[0]=0;
+        cache[1]=0;
+        int c = n-2;
+        
+        for(int i=2;i<sqrt(n)+1;i++){
+            if(cache[i]){
+                for(int j=i*i;j<n;j+=i){
+                    if(cache[j]){
+                        cache[j] = 0; 
+                        c--;
+                    }
+                }
+           }
+        }
+        return c;
+    }
+};

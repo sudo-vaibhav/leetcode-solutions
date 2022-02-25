b01260dca14519c8209ab0eea0f5de032b12aeed
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-25 18:02:25 +0530

    Time: 6 ms (8.87%), Space: 6.4 MB (10.49%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -1,48 +1,83 @@
+// Approach one using additional vector
+// class Solution {
+// public:
+//     int populateNumVec(string version, vector<int>& num){
+//         int temp = 0;
+//         for(auto i:version){
+//             if(isdigit(i)){
+//                 temp=temp*10+(i-'0');
+//             }
+//             else{
+//                 num.push_back(temp);
+//                 temp = 0;
+//             }
+//         }
+        
+//         num.push_back(temp);
+//         return num.size();
+//     }
+//     int compareVersion(string version1, string version2) {
+//         vector<int> num1,num2;
+//         int N = populateNumVec(version1,num1);
+//         int M = populateNumVec(version2,num2);
+        
+//         int i = 0;
+//         while(i<max(N,M)){
+//             auto v1 = 0;
+//             auto v2 = 0;
+            
+//             if(i<N){
+//                 v1 = num1[i];
+//             }
+//             if(i<M){
+//                 v2 = num2[i];
+//             }
+            
+//             cout<<v1<<" "<<v2<<"\n";
+//             if(v1>v2){
+//                 return 1;
+//             }
+//             else if(v2>v1){
+//                 return -1;
+//             }
+            
+//             i++;
+//         }
+        
+//         return 0;
+//     }
+// };
+
+
+// generator pattern, O(1) space
 class Solution {
 public:
-    int populateNumVec(string version, vector<int>& num){
-        int temp = 0;
-        for(auto i:version){
-            if(isdigit(i)){
-                temp=temp*10+(i-'0');
+   
+    
+    int getNextBlock(string version, int& idx, int N){
+        int num = 0;
+        if(idx==N) return 0;
+        for(;idx<N;idx++){
+            if(version[idx]=='.'){
+                idx++;
+                return num;
             }
             else{
-                num.push_back(temp);
-                temp = 0;
+                num = num*10 + (version[idx]-'0');
             }
         }
         
-        num.push_back(temp);
-        return num.size();
+        return num;
     }
-    int compareVersion(string version1, string version2) {
-        vector<int> num1,num2;
-        int N = populateNumVec(version1,num1);
-        int M = populateNumVec(version2,num2);
-        
-        int i = 0;
-        while(i<max(N,M)){
-            auto v1 = 0;
-            auto v2 = 0;
-            
-            if(i<N){
-                v1 = num1[i];
-            }
-            if(i<M){
-                v2 = num2[i];
-            }
-            
-            cout<<v1<<" "<<v2<<"\n";
-            if(v1>v2){
-                return 1;
-            }
-            else if(v2>v1){
-                return -1;
-            }
-            
-            i++;
+    int compareVersion(string version1, string version2){
+        int i=0,j=0,n=version1.size(),m = version2.size();
+        while(i<n||j<m){
+            auto num1 = getNextBlock(version1,i,n);
+            auto num2 = getNextBlock(version2,j,m);
+            cout<<num1<<" "<<num2<<endl;
+            if(num1<num2) return -1;
+            else if(num1>num2) return 1;
         }
-        
-        return 0;
+       return 0;
     }
 };

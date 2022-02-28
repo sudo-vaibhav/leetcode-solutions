a525ad7f33f3615d65e5511c4ebfafafffb78b8e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-28 18:15:41 +0530

    Time: 244 ms (5.28%), Space: 24.8 MB (42.65%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int zCount = 0;
+        int prod = accumulate(nums.begin(),nums.end(),1, [&zCount](int a, int b) mutable{
+            int ans = a;
+            if(b!=0){
+                ans *=b; 
+            }
+            else{
+                cout<<"this runs"<<endl;
+                zCount++;
+            }
+            cout<<a<<" "<<b<<endl;
+            return ans;
+        });
+        cout<<"zCount"<<zCount<<endl;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(zCount>1){
+                ans.push_back(0);
+            }
+            else if(zCount==1){
+                ans.push_back(nums[i]==0? prod: 0);
+            }
+            else{
+                if(nums[i]!=0)
+                ans.push_back(prod/nums[i]);
+            }
+        }
+        
+        return ans;
+        
+    }
+};

e242b22f8511eab5db0a981465709c9d8c51de8b
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-02-28 14:35:46 +0530

    Time: 0 ms (100.00%), Space: 7 MB (14.46%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        if(nums.size()==0) return {};
+        int intervalTarget = nums[0];
+        int intervalStart = nums[0];
+        int count=0;
+        int n = nums.size();
+        vector<string> ans;
+        for(int i=0;i<n;i++){
+            auto cur = nums[i];
+            if(abs((long)intervalTarget-(long)cur)<=1){
+                intervalTarget = cur;
+                count++;
+            }
+            else{
+                if(count>1){
+                    ans.push_back(to_string(intervalStart) +"->"+to_string(nums[i-1]));
+                    
+                }
+                else{
+                    ans.push_back(to_string(intervalStart));
+                }
+                intervalStart = cur;
+                intervalTarget = cur;
+                count=1;
+                
+            }
+        }
+        
+        if(count>1){
+                    ans.push_back(to_string(intervalStart) +"->"+to_string(nums[n-1]));
+                    
+                }
+                else{
+                    ans.push_back(to_string(intervalStart));
+                }
+        return ans;
+    }
+};

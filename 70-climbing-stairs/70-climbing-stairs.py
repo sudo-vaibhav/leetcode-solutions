b29fc07e23b1c178948c2a86ab58361345e0f814
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-01 21:16:56 +0530

    Time: 5371 ms (5.25%), Space: 13.9 MB (81.03%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,4 +1,4 @@
 class Solution:
-    @lru_cache(maxsize=5)
+    @lru_cache(maxsize=2)
     def climbStairs(self, n: int) -> int:
         return n if n <= 2 else self.climbStairs(n-1)+self.climbStairs(n-2)

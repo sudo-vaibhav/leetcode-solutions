f652c0b80ae49b0260795c16ad6a08fb6bd6168f
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-02 15:29:58 +0530

    Time: 72 ms (11.12%), Space: 24.4 MB (5.04%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,6 +1,5 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        @cache
         def solve(sIdx, tIdx, s, t):
             if(sIdx==len(s)): return True
             if(tIdx==len(t)): return False
@@ -10,7 +9,4 @@ class Solution:
                 return solve(sIdx+1,tIdx+1,s,t)
             else:
                 return solve(sIdx,tIdx+1,s,t)
-                
-            
-            
         return solve(0,0,s,t)

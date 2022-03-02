cebe48fc03c4dcfa0a615765bae2cc231cc3b9eb
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-03 02:08:05 +0530

    Time: 60 ms (5.01%), Space: 14.9 MB (7.83%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-       
         map<int,set<int>> adj;
-        
-        auto canReach = []  (int node, int target,map<int,set<int>>& adj) mutable {
-            // visited[i] = true;
+        auto canReach = [&](int node, int target) {
             queue<int> q;
             q.push(node);
             set<int> visited;  
             while(!q.empty()){
                 auto size = q.size();
-                
-                for(auto i=0;i<size;i++){
+                while(size--){
                     auto cur = q.front();
                     q.pop();
                     visited.insert(cur);
-                    if(cur==target){
-                        return true;
-                    }
+                    if(cur==target) return true;
                     else{
                         for(auto neighbor: adj[cur]){
                             if(!visited.count(neighbor)){
@@ -30,20 +24,17 @@ public:
                 }
                 
             }
-            
             return false;
         };
         
         for (auto edge:edges){
-            // visited = set<int>();
             if(
                adj.count(edge[0])
                && adj.count(edge[1])
-               && canReach(edge[0],edge[1],adj)
+               && canReach(edge[0],edge[1])
             ){
                 return edge;
             }
-            
             adj[edge[0]].insert(edge[1]);
             adj[edge[1]].insert(edge[0]);
         }

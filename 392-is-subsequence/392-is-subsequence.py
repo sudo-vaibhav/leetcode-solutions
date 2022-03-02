3966ac0f2135a015e88e4624a44d2a676ac915db
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-02 15:31:31 +0530

    Time: 36 ms (78.02%), Space: 14 MB (73.22%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,12 +1,25 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        def solve(sIdx, tIdx, s, t):
+        # def solve(sIdx, tIdx, s, t):
+        #     if(sIdx==len(s)): return True
+        #     if(tIdx==len(t)): return False
+        #     sVal = s[sIdx]
+        #     tVal = t[tIdx]
+        #     if(tVal==sVal):
+        #         return solve(sIdx+1,tIdx+1,s,t)
+        #     else:
+        #         return solve(sIdx,tIdx+1,s,t)
+        # return solve(0,0,s,t)
+        sIdx = 0
+        tIdx = 0
+        
+        while True:
             if(sIdx==len(s)): return True
             if(tIdx==len(t)): return False
             sVal = s[sIdx]
             tVal = t[tIdx]
             if(tVal==sVal):
-                return solve(sIdx+1,tIdx+1,s,t)
+                sIdx+=1
+                tIdx+=1
             else:
-                return solve(sIdx,tIdx+1,s,t)
-        return solve(0,0,s,t)
+                tIdx+=1

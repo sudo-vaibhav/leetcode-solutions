698587c01f31a9b0350f5839e736327e1002a310
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-04 23:17:45 +0530

    Time: 86 ms (14.36%), Space: 22.9 MB (8.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -15,28 +15,19 @@ public:
                      // now we do bfs and mark nearby connected parts
                     queue<pair<int,int>> q;
                     q.push({r,c});
-
                     while(!q.empty()){
-                        int temp = q.size();
-                        while(temp--){
-                            auto cur = q.front();
-                            q.pop();
-                            vector<pair<int,int>> moves = {{-1,0},{1,0},{0,1},{0,-1}};
-                            for(auto move:moves){
-                                auto R = cur.first+move.first;
-                                auto C = cur.second+move.second;
-                                if(R>=0&&R<m && C>=0 && C<n){
-                                    if(grid[R][C]=='1'&&!visited[R][C]){
-                                        visited[R][C] = 1;    
-                                        q.push({R,C});
-                                    }
-                                }
+                        auto cur = q.front();
+                        q.pop();
+                        vector<pair<int,int>> moves = {{-1,0},{1,0},{0,1},{0,-1}};
+                        for(auto move:moves){
+                            auto R = cur.first+move.first;
+                            auto C = cur.second+move.second;
+                            if(R>=0&&R<m && C>=0 && C<n && grid[R][C]=='1'&&!visited[R][C]){
+                                visited[R][C] = 1;    
+                                q.push({R,C});
                             }
                         }
-
                     }
-                    
-                   
                 }
             }
         }

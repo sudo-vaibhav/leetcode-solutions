03ede3ef5b77ff421134a4c871965801e147bde5
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-06 20:59:22 +0530

    Time: 178 ms (9.38%), Space: 22.1 MB (13.79%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
-//     int solve(int i1, int i2, string word1,string word2,vector<vector<int>>& dp){
-//         if(dp[i1][i2]!=-1) return dp[i1][i2];
-//         if(i1==0 || i2==0){
-//             return max(i1,i2);
-//         }
-//         else{
-            
-//         }
-//     }
-    
     int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-        for(auto i=0;i<=word1.size();i++){
-            dp[i][0] = i;
-        }
-        for(auto i=0;i<=word2.size();i++){
-            dp[0][i] = i;
-        }
-        for(int i1=1;i1<=word1.size();i1++){
-            for(int i2=1;i2<=word2.size();i2++){
-                if(word1[i1-1]==word2[i2-1]){
+        for(int i1=0;i1<=word1.size();i1++){
+            for(int i2=0;i2<=word2.size();i2++){
+                if(i2==0) dp[i1][0] = i1;
+                else if(i1==0) dp[0][i2] = i2;
+                else if(word1[i1-1]==word2[i2-1]){
                     dp[i1][i2] = dp[i1-1][i2-1];
                 }
                 else{

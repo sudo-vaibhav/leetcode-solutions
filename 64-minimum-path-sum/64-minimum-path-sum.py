44a9a59b6e6508f06078e40576d71b5cf78bc4c0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-08 19:09:02 +0530

    Time: 163 ms (38.36%), Space: 16 MB (24.05%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,11 +1,25 @@
+# class Solution:
+#     def minPathSum(self, grid: List[List[int]]) -> int:
+#         m = len(grid)
+#         n = len(grid[0])
+#         @cache
+#         def solve(i,j):
+#             if(i==m-1 and j==n-1): return grid[i][j]
+#             if i==m or j==n : return float("inf")
+#             return grid[i][j]+min(solve(i+1,j),solve(i,j+1))
+#         return int(solve(0,0))
+
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        @cache
-        def solve(i,j):
-            if(i==m-1 and j==n-1): return grid[i][j]
-            if i==m or j==n : return float("inf")
-            return grid[i][j]+min(solve(i+1,j),solve(i,j+1))
+        dp = [[float("inf") for j in range(n+1)] for i in range(m+1)]
+        dp[m-1][n-1] = grid[m-1][n-1]
         
-        return int(solve(0,0))
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if m-1==i and n-1==j: continue
+                dp[i][j] = grid[i][j] + min(dp[i+1][j],dp[i][j+1])
+        # for l in dp:
+        #     print(*l)
+        return dp[0][0]

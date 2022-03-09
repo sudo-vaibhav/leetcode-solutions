cf88ebf56bfd2022449dca4bbefc90f4b1e433ec
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-09 23:24:40 +0530

    Time: 24 ms (38.75%), Space: 13.9 MB (27.46%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // both inclusive
+    int dp[305][305];
+    bool solve(int start, int end,string&s,  unordered_set<string>&words){
+        if(start>end) return 1;
+        if(dp[start][end]!=-1) return dp[start][end];
+        // for(auto BEGIN=start;BEGIN<=end;BEGIN++){
+            for(auto END=start;END<=end;END++){
+                auto substring = s.substr(start,END-start+1);
+                if(
+                    words.count(substring)
+                    // && solve(start,BEGIN-1,s,words)
+                    && solve(END+1,end,s,words)
+                ){
+                    return dp[start][end]=1;
+                }
+                
+            }
+        // }
+        
+        return dp[start][end]=0;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        memset(dp,-1,sizeof(dp));
+        unordered_set<string> words(wordDict.begin(),wordDict.end());
+        return solve(0,s.size()-1,s,words);
+    }
+};

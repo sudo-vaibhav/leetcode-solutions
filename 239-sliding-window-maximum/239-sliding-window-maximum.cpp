1c68d6720f5bacf7423e4006cb6913a4b216231d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-12 17:50:15 +0530

    Time: 248 ms (89.92%), Space: 134 MB (38.56%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,37 +1,11 @@
 // another naive approach would have been to check elem by elem for every possible window (Brute force)
 
 // O(n) time and O(n) space soln using deque
-// class Solution {
-// public:
-//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-//         // pair -> index , val
-//         deque<pair<int,int>> q;
-//         vector<int> ans;
-//         for(int i=0;i<nums.size();i++){
-//             auto cur = nums[i];
-//             while(!q.empty() && q.front().first<i-k+1){
-//                 q.pop_front();
-//             }
-            
-//             while(!q.empty()&&q.back().second<=cur){
-//                 q.pop_back();
-//             }
-            
-//             q.push_back({i,cur});
-//             if(i>=k-1){
-//                 ans.push_back(q.front().second);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         // pair -> index , val
-        list<pair<int,int>> q;
+        deque<pair<int,int>> q;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
             auto cur = nums[i];
@@ -51,4 +25,3 @@ public:
         return ans;
     }
 };
-

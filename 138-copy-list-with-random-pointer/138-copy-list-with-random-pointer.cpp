6784a2077452f5f5d8c5258031a5d92c6a28f8b4
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-12 13:32:57 +0530

    Time: 30 ms (5.95%), Space: 11.4 MB (27.27%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -14,41 +14,124 @@ public:
 };
 */
 
-class Solution {
-public:
-    Node* copyRandomList(Node* head) {
-        auto dummy = new Node(0);
-        auto it = dummy;
+// O(n) time O(n) space
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//         auto dummy = new Node(0);
+//         auto it = dummy;
+//         auto temp = head;
+//         map<Node*,Node*> cache;
+        
+        
+//         while(temp){
+//             if(cache.count(temp)){
+//                 it->next = cache[temp];
+//             }
+//             else{
+//                 it->next = new Node(temp->val);
+//                 cache[temp] = it->next;
+//             }
+            
+//             if(temp->random==NULL) {
+//                 it->next->random = NULL;
+//             }
+//             else if(cache.count(temp->random)){
+//                 it->next->random = cache[temp->random];
+//             }
+//             else{
+//                 cache[temp->random] = new Node(temp->random->val);
+//                 cache[temp->random]->next = temp->random->next;
+//                 it->next->random = cache[temp->random];
+//             }
+            
+//             it = it->next;
+//             temp = temp->next;
+            
+//         }
+        
+//         return dummy->next;
+//     }
+// };
+
+class Solution{
+    public:
+    void printList(Node* head){
         auto temp = head;
-        map<Node*,Node*> cache;
-        
-        
         while(temp){
-            if(cache.count(temp)){
-                it->next = cache[temp];
+            cout<<temp->val;
+            if(temp->random){
+                cout<<" ("<<temp->random->val<<")";
             }
-            else{
-                it->next = new Node(temp->val);
-                cache[temp] = it->next;
-            }
-            
-            if(temp->random==NULL) {
-                it->next->random = NULL;
-            }
-            else if(cache.count(temp->random)){
-                it->next->random = cache[temp->random];
-            }
-            else{
-                cache[temp->random] = new Node(temp->random->val);
-                cache[temp->random]->next = temp->random->next;
-                it->next->random = cache[temp->random];
-            }
-            
-            it = it->next;
+            cout<<"\t";
             temp = temp->next;
+        }
+        cout<<endl;
+    }
+    
+    Node* copyRandomList(Node* head) {
+        if(!head) return NULL;
+        
+        // step1 - create new elements and place them adjacently
+        auto temp = head;
+        while(temp){
+            auto nextNode = temp->next;
+            auto copyNode = new Node(temp->val);
+            copyNode->next = nextNode;
+            temp->next = copyNode;
+            temp = copyNode->next;
+        }
+        
+        printList(head);
+        
+        // step2 - now establish random pointers
+        auto oldIter = head;
+        auto newIter = head->next;
+        while(true){
+            if(oldIter->random==NULL) newIter->random = NULL;
+            else{
+                newIter->random = oldIter->random->next;    
+            }
+            
+            oldIter = oldIter->next->next;
+            if(newIter->next){
+                newIter = newIter->next->next;
+            }
+            else{
+                break;
+            }
             
         }
         
-        return dummy->next;
+        printList(head);
+        
+        // step3 - establish next pointers and restore both old and new lists to final form
+        oldIter = head;
+        newIter = head->next;
+        auto ans = newIter;
+        while(newIter){
+            auto nextOldNode = newIter->next;
+            Node* nextNewNode;
+            if(!nextOldNode){
+                newIter->next = NULL;
+                oldIter->next = NULL;
+                break;
+            }
+            else{
+                nextNewNode = nextOldNode->next;
+            }
+            
+            
+            oldIter->next = nextOldNode;
+            newIter->next = nextNewNode;
+            
+            newIter = newIter->next;
+            oldIter = oldIter->next;
+        }
+        
+        cout<<"final:\n";
+        printList(head);
+        printList(ans);
+        return ans;
     }
 };

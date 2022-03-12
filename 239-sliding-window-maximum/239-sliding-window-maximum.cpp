f391e4dc61da2c812412dd4e6988826879f7a553
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-12 17:47:54 +0530

    Time: 333 ms (75.93%), Space: 133.9 MB (39.40%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -0,0 +1,26 @@
+// another naive approach would have been to check elem by elem for every possible window (Brute force)
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // pair -> index , val
+        deque<pair<int,int>> q;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            auto cur = nums[i];
+            while(!q.empty() && q.front().first<i-k+1){
+                q.pop_front();
+            }
+            
+            while(!q.empty()&&q.back().second<=cur){
+                q.pop_back();
+            }
+            
+            q.push_back({i,cur});
+            if(i>=k-1){
+                ans.push_back(q.front().second);
+            }
+        }
+        return ans;
+    }
+};

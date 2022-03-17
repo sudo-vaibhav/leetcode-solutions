d07a1460c2d98b92339b24eedcb1a1d13102d763
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-17 23:47:50 +0530

    Time: 5 ms (14.59%), Space: 5.9 MB (99.93%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,24 +1,43 @@
+// class Solution {
+// public:
+//     int scoreOfParentheses(string s) {
+//         stack<int> scoreAtLevel;
+//         scoreAtLevel.push(0); // overall level
+//         for(auto c:s){
+//             if(c=='('){
+//                 scoreAtLevel.push(0);
+//             }
+//             else{
+//                 auto v = scoreAtLevel.top();
+//                 scoreAtLevel.pop();
+//                 auto newV = scoreAtLevel.top()+max(2*v,1);
+//                 scoreAtLevel.pop();
+//                 scoreAtLevel.push(newV);
+//             }
+//         }
+        
+//         return scoreAtLevel.top();
+        
+//     }
+// };
+
 class Solution {
 public:
-//     (()())
-//     
     int scoreOfParentheses(string s) {
-        stack<int> scoreAtLevel;
-        scoreAtLevel.push(0); // overall level
-        for(auto c:s){
+        int ans = 0,outerParenCount=0;
+        for(auto i=0;i<s.size();i++){
+            auto c = s[i];
             if(c=='('){
-                scoreAtLevel.push(0);
+                outerParenCount++;
             }
             else{
-                auto v = scoreAtLevel.top();
-                scoreAtLevel.pop();
-                auto newV = scoreAtLevel.top()+max(2*v,1);
-                scoreAtLevel.pop();
-                scoreAtLevel.push(newV);
+                outerParenCount--;
+                if(s[i-1]=='('){
+                    ans += 1<<outerParenCount;
+                }
             }
         }
-        
-        return scoreAtLevel.top();
+        return ans;
         
     }
 };

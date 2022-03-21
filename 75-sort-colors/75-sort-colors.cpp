bddb33eb6b00ac33b1c649f8d90f4af4a2d98215
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-21 23:19:23 +0530

    Time: 4 ms (46.15%), Space: 8.4 MB (24.64%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,26 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//     }
+// };
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int lo = 0,hi = nums.size()-1,mid = 0;
+        while(mid<=hi){
+            switch(nums[mid]){
+                case 0:
+                    swap(nums[mid++],nums[lo++]);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                default:
+                    swap(nums[mid],nums[hi--]);
+                    
+            }
+        }
     }
 };

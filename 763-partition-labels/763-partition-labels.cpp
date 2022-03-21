26add4a11829041a83907554248daf8dcb863265
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-21 09:29:44 +0530

    Time: 8 ms (32.00%), Space: 7 MB (10.11%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -2,32 +2,17 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         unordered_map<char,pair<int,int>> m;
-        int t = 0;
-        for (auto i:s){
-            if(m.count(i)){
-                m[i] = {m[i].first,t};
-            }
-            else{
-                m[i] = {t,t};
-            }
-            t++;
-        }
-        
         vector<pair<int,int>> intervals;
-        for(auto i:m){
-            intervals.push_back({i.second});
+        for (int i=0;i<s.size();i++){
+            if(m.count(s[i])) m[s[i]] = {m[s[i]].first,i};
+            else m[s[i]] = {i,i};
         }
         
-        sort(intervals.begin(),intervals.end(),[](pair<int,int> i1,pair<int,int> i2){
-            // if(i1.first!=i2.first){
-                return i1.first<i2.first;
-            // }
-            // else{
-                // return i1.second<i2.second;
-            // }
-        });
+        for(auto i:m) intervals.push_back({i.second});
+        sort(intervals.begin(),intervals.end());
         vector<pair<int,int>> ans;
         pair<int,int> prev = intervals[0];
+        
         for(int i=1;i<intervals.size();i++){
             auto cur = intervals[i];
             if(cur.first<prev.second){
@@ -38,13 +23,10 @@ public:
                 prev = cur;
             }
         }
-        
         // also account for prev
         ans.push_back(prev);
         vector<int> res = vector<int>(ans.size()); 
-        for(int i=0;i<ans.size();i++){
-            res[i] = ans[i].second-ans[i].first+1;
-        }
+        for(int i=0;i<ans.size();i++) res[i] = ans[i].second-ans[i].first+1;
         return res;
     }
 };

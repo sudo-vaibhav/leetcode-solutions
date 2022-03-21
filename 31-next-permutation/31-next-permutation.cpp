fd0568cd53c3d2a3e26a6f8a87b9d6731c76e7f6
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-21 23:02:20 +0530

    Time: 8 ms (39.77%), Space: 12.2 MB (37.37%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,36 @@
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         next_permutation(nums.begin(),nums.end());
+//     }
+// };
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n = nums.size();
+        int temp = INT_MAX;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                temp = i;
+                break;
+            }
+        }
+        
+        if(temp==INT_MAX){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            auto v = nums[temp];
+            int i=n-1;
+            for(;i>temp;i--){
+                if(nums[i]>v){
+                    swap(nums[temp],nums[i]);
+                    break;
+                }
+            }
+            
+            reverse(nums.begin()+temp+1,nums.end());
+        }
     }
 };

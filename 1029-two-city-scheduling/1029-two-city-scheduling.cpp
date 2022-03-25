5c3e9a53016c1fe3fd808b80408366179d1e2424
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-25 10:39:44 +0530

    Time: 8 ms (42.77%), Space: 8.3 MB (25.59%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,30 +1,58 @@
-class Solution {
-public:
-    map<vector<int>,int> dp;
-    int solve(int t,int pl, int pr,vector<vector<int>>& costs){
-        if(dp.count({t,pl,pr})) return dp[{t,pl,pr}];
-        if(pl==0 && pr==0) return dp[{t,pl,pr}]= 0;
-        int s=0;
-        if(pl==0){
-            for(int i=0;i<t;i++){
-                s+=costs[i][1];
-            }
-            return dp[{t,pl,pr}]=s;
-        }
-        else if(pr==0){
-            for(int i=0;i<t;i++){
-                s+=costs[i][0];
-            }
-            return dp[{t,pl,pr}]=s;
-        }
-        return dp[{t,pl,pr}]=min(costs[t-1][0]+solve(t-1,pl-1,pr,costs),
-            costs[t-1][1]+solve(t-1,pl,pr-1,costs)
-           );
-    }
+// Recursive 3D DP solution
+// class Solution {
+// public:
+//     map<vector<int>,int> dp;
+//     int solve(int t,int pl, int pr,vector<vector<int>>& costs){
+//         if(dp.count({t,pl,pr})) return dp[{t,pl,pr}];
+//         if(pl==0 && pr==0) return dp[{t,pl,pr}]= 0;
+//         int s=0;
+//         if(pl==0){
+//             for(int i=0;i<t;i++){
+//                 s+=costs[i][1];
+//             }
+//             return dp[{t,pl,pr}]=s;
+//         }
+//         else if(pr==0){
+//             for(int i=0;i<t;i++){
+//                 s+=costs[i][0];
+//             }
+//             return dp[{t,pl,pr}]=s;
+//         }
+//         return dp[{t,pl,pr}]=min(costs[t-1][0]+solve(t-1,pl-1,pr,costs),
+//             costs[t-1][1]+solve(t-1,pl,pr-1,costs)
+//            );
+//     }
     
-    int twoCitySchedCost(vector<vector<int>>& costs) {
-        int n = costs.size()/2;
-        dp = map<vector<int>,int>();
-        return solve(2*n,n,n,costs);
+//     int twoCitySchedCost(vector<vector<int>>& costs) {
+//         int n = costs.size()/2;
+//         dp = map<vector<int>,int>();
+//         return solve(2*n,n,n,costs);
+//     }
+// };
+
+
+// refund concept
+class Solution{
+    public:
+  int twoCitySchedCost(vector<vector<int>>& costs) {
+        int n = costs.size();
+        vector<int> init = {0, 0};
+        auto cumulationRes = accumulate(costs.begin(), costs.end(), init, [](vector<int> X, vector<int> Y)
+                                        {
+                                           vector<int> res= {X[0] + Y[0], 0};
+                                            return res; });
+        int cost = cumulationRes[0];
+        vector<int> benefitsOfTransfering(n);
+        for (int i = 0; i < n; i++)
+        {
+            benefitsOfTransfering[i] = costs[i][0] - costs[i][1];
+        }
+
+        sort(benefitsOfTransfering.begin(), benefitsOfTransfering.end());
+        for (int i = n / 2; i < n; i++)
+        {
+            cost -= benefitsOfTransfering[i];
+        }
+        return cost;
     }
 };

46ac157ee2a44925d45baec80e1b9dd26f077222
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-03-25 10:58:51 +0530

    Time: 4 ms (78.75%), Space: 8.8 MB (20.34%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -72,13 +72,13 @@ class Solution{
       });
       
       int s = 0;
-      for (int i=0;i<n;i++){
-          if(i<n/2){
-              s+=costs[i][0];
-          }
-          else{
-              s+=costs[i][1];
-          }
+      for (int i=0;i<n/2;i++){
+          // if(i<n/2){
+              s+=costs[i][0]+costs[i+n/2][1];
+          // }
+          // else{
+              // s+=;
+          // }
       }
       return s;
     }

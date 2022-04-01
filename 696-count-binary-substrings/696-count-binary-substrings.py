c3a6bde3cf3b8df8cde2569e1e76a656ca428a82
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-01 21:17:49 +0530

    Time: 171 ms (86.69%), Space: 14.5 MB (45.26%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -1,16 +1,37 @@
+# class Solution:
+#     def countBinarySubstrings(self, s: str) -> int:
+#         continuousCounts = []
+#         prev = "-"
+#         for i in s:
+#             if i!=prev:
+#                 continuousCounts.append(1)
+#                 prev = i
+#             else:
+#                 continuousCounts[-1]+=1
+#         s=0
+#         for i in range(1,len(continuousCounts)):
+#             s+=min(continuousCounts[i],continuousCounts[i-1])
+#         return s
+
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
         continuousCounts = []
         prev = "-"
+        prevC = 0
+        curC = 0
+        ans=0
         for i in s:
             if i!=prev:
-                continuousCounts.append(1)
+                ans+=min(curC,prevC)
+                prevC = curC
+                curC=1
                 prev = i
             else:
-                continuousCounts[-1]+=1
-        s=0
-        for i in range(1,len(continuousCounts)):
-            s+=min(continuousCounts[i],continuousCounts[i-1])
-            
-        return s
-                
+                curC+=1
+                # continuousCounts[-1]+=1
+       
+        # for i in range(1,len(continuousCounts)):
+        #     s+=min(continuousCounts[i],continuousCounts[i-1])
+        
+        ans+=min(prevC,curC)
+        return ans

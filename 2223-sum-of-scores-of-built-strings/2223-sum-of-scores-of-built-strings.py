7aa149caa1e916c074a4338e4072f1ea8592b7a6
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-05 16:01:08 +0530

    Time: 800 ms (56.39%), Space: 18.1 MB (91.48%) - LeetHub

--- 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.py
+++ 2223-sum-of-scores-of-built-strings/2223-sum-of-scores-of-built-strings.py
@@ -1,52 +1,26 @@
 class Solution:
     def sumScores(self, s: str) -> int:
-        def getZarr(string, z):
-            n = len(string)
+        n = len(s)
+        def getZarr(s, z):
+            l = 0
+            r = 0
+            for i in range(1,n):
+#                 kick start step
+                if i<=r:
+                    z[i] = min(z[i-l],r-i+1)
+                
+# advance ment step
+                while i+z[i]<n and s[z[i]]==s[i+z[i]]:
+                    z[i]+=1
+            
+                if i+z[i]-1>r:
+                    l = i
+                    r = i+z[i]-1
+            
+# limit recallibration step
 
-            # [L,R] make a window which matches
-            # with prefix of s
-            l, r, k = 0, 0, 0
-            for i in range(1, n):
-
-                # if i>R nothing matches so we will calculate.
-                # Z[i] using naive way.
-                if i > r:
-                    l, r = i, i
-
-                    # R-L = 0 in starting, so it will start
-                    # checking from 0'th index. For example,
-                    # for "ababab" and i = 1, the value of R
-                    # remains 0 and Z[i] becomes 0. For string
-                    # "aaaaaa" and i = 1, Z[i] and R become 5
-                    while r < n and string[r - l] == string[r]:
-                        r += 1
-                    z[i] = r - l
-                    r -= 1
-                else:
-
-                    # k = i-L so k corresponds to number which
-                    # matches in [L,R] interval.
-                    k = i - l
-
-                    # if Z[k] is less than remaining interval
-                    # then Z[i] will be equal to Z[k].
-                    # For example, str = "ababab", i = 3, R = 5
-                    # and L = 2
-                    if z[k] < r - i + 1:
-                        z[i] = z[k]
-
-                    # For example str = "aaaaaa" and i = 2,
-                    # R is 5, L is 0
-                    else:
-
-                        # else start from R and check manually
-                        l = i
-                        while r < n and string[r - l] == string[r]:
-                            r += 1
-                        z[i] = r - l
-                        r -= 1
-        z = [len(s)]*len(s)
+        z = [0]*n
         getZarr(s,z)
         # print(z)
-        return sum(z)
+        return n+sum(z)
  

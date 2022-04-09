d5b924c3cd4a831c00a82bc5d4955c5a2de54a4d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-09 09:10:42 +0530

    Time: 32 ms (17.99%), Space: 13.6 MB (71.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
-        for(auto i:nums){
-            m[i]++;
-        }
-        vector<pair<int,int>> f;
-        for(auto i:m){
-            f.push_back({i.second,i.first});
-        }
-        nth_element(f.begin(),f.begin()+f.size()-k,f.end());
-        vector<int>ans;
-        int n = f.size();
-        for(int i=n-1;i>=n-k;i--){
-            ans.push_back(f[i].second);
-        }
-        return ans;
+        unordered_map<int,int> m; // create a frequency map
+        for(auto i:nums) m[i]++; // populate the map
+        vector<pair<int,int>> f; // declare a vector and have frequency as sort key
+        for(auto i:m) f.push_back({i.second,i.first}); // populate the freq vector
+        nth_element(f.begin(),f.begin()+f.size()-k,f.end()); // qk select to have k largest numbers in place (but not sorted)
+        vector<int>ans; // answer vector
+        int n = f.size(); // n
+        for(int i=n-1;i>=n-k;i--) ans.push_back(f[i].second); // get k most frequent in answer
+        return ans; // return ans
     }
 };

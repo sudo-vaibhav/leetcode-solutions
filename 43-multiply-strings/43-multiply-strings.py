a63af72097fa10ea1addad39f46971ad1a3a6776
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-11 00:25:42 +0530

    Time: 78 ms (55.63%), Space: 13.9 MB (77.79%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,3 +1,9 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        ans = 0
+        for idx,b in enumerate(num2[::-1]):
+            base = 10**idx
+            tempans = int(num1)*int(b)
+            ans+=base*tempans
+            
+        return str(ans)

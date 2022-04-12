0652d61627e018fd5b552d5409ddec65f823c3b7
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-12 20:36:55 +0530

    Time: 196 ms (22.15%), Space: 58 MB (5.08%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -9,6 +9,7 @@ public:
         int m=grid.size(),n=grid[0].size();
         vector<vector<bool>> visited(m,vector<bool>(n,false));
         vector<vector<vector<int>>> ans;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if (!visited[i][j] && grid[i][j]==1){
@@ -32,19 +33,52 @@ public:
                 
             }
         }
-        // cout<<ans.size();
         return ans;
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
         auto islands = getIslands(grid);
-        int ans = INT_MAX;
-        for(auto& land : islands[0]){
-            for(auto& land2:islands[1]){
-                ans = min(ans,manDist(land,land2));
-            }
+        int m=grid.size(),n=grid[0].size();
+        // int ans = INT_MAX;
+        queue<pair<int,int>> q;
+        for(auto& t:islands[0]){
+            // cout<<t[0]<<" "<<t[1]<<"\n";
+            q.push({t[0],t[1]});
         }
-        
-        return ans-1;
+        set<pair<int,int>> s;
+        // cout<<"second island\n";
+        for(auto& land:islands[1]){
+            s.insert({land[0],land[1]});
+        }
+        // cout<<"checking set\n";
+        // for(auto& e:s){
+        //     cout<<e.first<<" "<<e.second<<"\n";
+        // }
+        int dist = 0;
+        // for(auto& row:grid){
+        //     for(auto col:row){
+        //         cout<<col<<" ";
+        //     }
+        //     // cout<<"\n";
+        // }
+        while(!q.empty()){
+            int qSize = q.size();
+            for(int i=0;i<qSize;i++){
+                auto cur = q.front();q.pop();
+                // cout<<cur.first<<" "<<cur.second<<"\n";
+                for(const auto move:moves){
+                    int I=cur.first+move[0],J=cur.second+move[1];
+                    
+                    if(s.count({I,J})) return dist;
+                    if(I>=0 && I<m && J>=0 && J<n && grid[I][J]==0){
+                        // cout<<I<<" "<<J<<"\n";
+                        grid[I][J]=1;
+                        q.push({I,J});
+                    }
+                }
+            }
+            dist++;
+        }
+        return -1;
     }
 };

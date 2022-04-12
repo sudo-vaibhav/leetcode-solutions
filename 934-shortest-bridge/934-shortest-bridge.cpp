72ad7c8c07f6dc0b39f5015049e715b85fc7d880
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-12 20:38:14 +0530

    Time: 329 ms (15.89%), Space: 57.9 MB (5.08%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -6,7 +6,7 @@ public:
     }
     const vector<vector<int>> moves=  {{-1,0},{1,0},{0,1},{0,-1}};
     vector<vector<vector<int>>> getIslands(const vector<vector<int>>& grid){
-        int m=grid.size(),n=grid[0].size();
+        const int m=grid.size(),n=grid[0].size();
         vector<vector<bool>> visited(m,vector<bool>(n,false));
         vector<vector<vector<int>>> ans;
         
@@ -37,41 +37,25 @@ public:
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
-        auto islands = getIslands(grid);
+        const auto islands = getIslands(grid);
         int m=grid.size(),n=grid[0].size();
-        // int ans = INT_MAX;
         queue<pair<int,int>> q;
         for(auto& t:islands[0]){
-            // cout<<t[0]<<" "<<t[1]<<"\n";
             q.push({t[0],t[1]});
         }
         set<pair<int,int>> s;
-        // cout<<"second island\n";
         for(auto& land:islands[1]){
             s.insert({land[0],land[1]});
         }
-        // cout<<"checking set\n";
-        // for(auto& e:s){
-        //     cout<<e.first<<" "<<e.second<<"\n";
-        // }
         int dist = 0;
-        // for(auto& row:grid){
-        //     for(auto col:row){
-        //         cout<<col<<" ";
-        //     }
-        //     // cout<<"\n";
-        // }
         while(!q.empty()){
             int qSize = q.size();
             for(int i=0;i<qSize;i++){
-                auto cur = q.front();q.pop();
-                // cout<<cur.first<<" "<<cur.second<<"\n";
+                const auto cur = q.front();q.pop();
                 for(const auto move:moves){
                     int I=cur.first+move[0],J=cur.second+move[1];
-                    
                     if(s.count({I,J})) return dist;
                     if(I>=0 && I<m && J>=0 && J<n && grid[I][J]==0){
-                        // cout<<I<<" "<<J<<"\n";
                         grid[I][J]=1;
                         q.push({I,J});
                     }

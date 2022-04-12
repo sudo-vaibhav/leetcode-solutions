21b2c017eb1ca572efa3470f4c53320b4ec843ef
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-12 21:35:55 +0530

    Time: 636 ms (58.07%), Space: 45.5 MB (28.48%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -5,27 +5,28 @@ class Solution:
             adj[c[0]].append(c[1])
             adj[c[1]].append(c[0])
         visited = [False for i in range(n)]
-        # print(adj)
         def dfs(comp,parent):
-            ew = 0
+            extraWires = 0
             for neig in adj[comp]:
                 if not visited[neig]:
                     visited[neig]=True
-                    ew+=dfs(neig,comp)
+                    extraWires+=dfs(neig,comp)
                 else:
-                    if neig != parent:
-                        ew+=1
-            return ew
+                    if neig != parent: 
+                        # all already visited computers contribute to additional wires except parent,
+                        # link to parent is obviously important otherwise one would have never reached this computer
+                        # and thus that can't be counted as extra
+                        extraWires+=1
+            return extraWires
                     
         connectedComponents = 0
-        ew=0
+        extraWires=0
         for comp in range(n):
             if not visited[comp]:
                 visited[comp] = True
-                ew+=dfs(comp,-1)//2
+                extraWires+=dfs(comp,-1)//2 # divide by two as graph is undirected and reports extra wire value twice
                 connectedComponents+=1
-        # print(ew)
-        if ew < connectedComponents-1:
+        if extraWires < connectedComponents-1:
             return -1
         else: return connectedComponents-1
         

870c2ca38664831599a09231f8d86268450314e6
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-13 13:21:17 +0530

    Time: 92 ms (88.09%), Space: 14.3 MB (42.22%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -11,12 +11,12 @@ class Solution:
         for e in blueEdges:
             adj[e[0]][1].append(e[1])            
         
-        def bfs(node,prevColor,seen=set()):
+        def bfs(node,seen=set()):
             distances = [inf for i in range(n)]
             distances[0] = 0
             q = deque() 
-            q.append((node,prevColor))
-            q.append((node,1-prevColor))
+            q.append((node,0))
+            q.append((node,1))
             dist = 0
             while q:
                 dist+=1
@@ -24,21 +24,19 @@ class Solution:
                 while qs:
                     qs-=1
                     cur = q.popleft()
-                    if (cur in seen):continue
                     seen.add(cur)
                     curNode,curColor = cur
                     newColor = 1-curColor
                     for neig in adj[curNode][newColor]:
-                        distances[neig] = min(dist,distances[neig])
-                        q.append((neig,newColor))
+                        if (neig,newColor) not in seen:
+                            seen.add((neig,newColor))
+                            distances[neig] = min(dist,distances[neig])
+                            q.append((neig,newColor))
             return distances
 
 
 
-        d1 = bfs(0,0)
-        # d2 = bfs(0,1)
-        
-        # print(adj,d1,d2,sep="\n")
+        d1 = bfs(0)
         ans = [-1 for i in range(n)]
         for i in range(n):
             temp = min(d1[i],d2[i]) 

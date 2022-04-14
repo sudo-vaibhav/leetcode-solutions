615222254c5a7a14544506c07918bef465503177
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-14 20:17:23 +0530

    Time: 757 ms (84.36%), Space: 23.2 MB (9.07%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.py
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -5,24 +5,23 @@ class Solution:
             adj[u].append(v)
             adj[v].append(u)
             
-        # print(adj)
-        self.possible = True
+        possible = True
         col = {i:-1 for i in range(1,n+1)}
         
         def dfs(i,color):
+            nonlocal possible
             col[i]=color
             for dest in adj[i]:
                 if col[dest]==-1:
                     dfs(dest,1-color)
 
                 elif col[dest]==color:
-                    self.possible = False
+                    possible = False
                     return 
             
         for i in range(1,n+1):
             if col[i]==-1:
                 dfs(i,0)
-        # print(col)       
-        return self.possible
+        return possible
         
         

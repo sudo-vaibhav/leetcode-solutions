48dcc2c6c895708294b177cfe2c70ed30a516628
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-15 22:13:04 +0530

    Time: 132 ms (42.10%), Space: 14.4 MB (7.96%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.py
@@ -1,7 +1,29 @@
+# simple approach using string concatenation
+# class Solution:
+#     def repeatedSubstringPattern(self, s: str) -> bool:
+#         return s in (s+s)[1:-1]
+
+
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        S = s+s
-        return s in S[1:-1]
+        n = len(s)
+        def lps(lpsArray):
+            i,length = 1,0
+            while i<n:
+                if s[length]==s[i]:
+                    length+=1
+                    lpsArray[i]=length
+                    i+=1
+                else:
+                    if length>0:
+                        length = lpsArray[length-1]
+                    else:
+                        lpsArray[i]=0
+                        i+=1
+            
         
-        
-        
+        lpsArray = [0 for i in range(n)]
+        lps(lpsArray)
+        if lpsArray[-1]==0: return False
+        temp = len(s)-lpsArray[-1]
+        return s[:temp]*(n//temp)==s

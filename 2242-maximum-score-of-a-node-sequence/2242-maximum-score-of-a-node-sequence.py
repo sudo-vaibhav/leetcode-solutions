85f29cbd3dc86ffe4d78b5e3d1000a33ba16d5ad
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-16 22:00:37 +0530

    Time: 2232 ms (33.33%), Space: 42.3 MB (66.67%) - LeetHub

--- 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.py
+++ 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.py
@@ -1,15 +1,11 @@
 class Solution:
     def maximumScore(self, scores: List[int], edges: List[List[int]]) -> int:
-        adj = defaultdict(list)
-        n  = len(scores)
+        adj,ans = defaultdict(list),-1
         for u,v in edges:
             adj[u].append((scores[v],v))
             adj[v].append((scores[u],u))
-        
-        for i in range(n):
+        for i in adj:
             adj[i] = nlargest(3,adj[i])
-            
-        ans = -1
         for b,c in edges:
             for (aW,a),(dW,d) in product(adj[b],adj[c]):
                 if a!=c and d!=b and a!=d:

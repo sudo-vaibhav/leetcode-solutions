080ea4c7c919578b661d4cf4b05d76fab1c1947b
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-19 12:11:10 +0530

    Time: 92 ms (58.86%), Space: 14.2 MB (94.87%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -1,31 +1,59 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-#         3 2 1
-#         1 3 2 4
+# class Solution:
+#     def recoverTree(self, root: Optional[TreeNode]) -> None:
+#         def lvr(root):
+#             if not root: return []
+#             return [*lvr(root.left),root,*lvr(root.right)]
+        
+#         trav = lvr(root)
+#         sted = sorted(trav,key=lambda x:x.val)
+#         viol1,viol2,n = inf,inf,len(trav)
+#         for i in range(n):
+#             if trav[i].val!=sted[i].val:
+#                 if viol1==inf:
+#                     viol1=trav[i]
+#                 else:
+#                     viol2=trav[i]
+#                     break
+#         viol1.val,viol2.val = viol2.val,viol1.val
 
+class Solution:
+    def __init__(self):
+        self.prev = TreeNode(float("-inf"))
+        self.first = None
+        self.second = None
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
         def lvr(root):
-            if not root: return []
-            return [*lvr(root.left),root,*lvr(root.right)]
+            if not root: return
+            lvr(root.left)
+            
+            if self.prev.val > root.val and not self.first:
+                self.first = self.prev
+            if self.prev.val > root.val and self.first:
+                self.second = root
+            self.prev = root
+            lvr(root.right)
         
-        trav = lvr(root)
-        sted = sorted(trav,key=lambda x:x.val)
-        viol1,viol2,n = inf,inf,len(trav)
-        for i in range(n):
-            if trav[i].val!=sted[i].val:
-                if viol1==inf:
-                    viol1=trav[i]
-                else:
-                    viol2=trav[i]
-                    break
-        viol1.val,viol2.val = viol2.val,viol1.val
+        lvr(root)    
+        self.first.val,self.second.val = self.second.val,self.first.val
+
+# class Solution:
+#     def __init__(self):
+#         self.first, self.second = None, None
+#         self.prev = TreeNode(float('-inf'))
         
+#     def recoverTree(self, root: TreeNode) -> None:
+#         """
+#         Do not return anything, modify root in-place instead.
+#         """
+#         def inorder(root):
+#             if root:
+#                 inorder(root.left)
+#                 if not self.first and self.prev.val >= root.val:
+#                     self.first = self.prev
+#                 if self.first and self.prev.val >= root.val:
+#                     self.second = root
+#                 self.prev = root
+#                 inorder(root.right)
         
+#         inorder(root)
+#         self.first.val, self.second.val = self.second.val, self.first.val

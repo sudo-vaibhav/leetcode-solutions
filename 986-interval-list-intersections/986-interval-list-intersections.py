c52cf9d53babd53e83b61f51b00051ceb49e9273
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-20 14:18:06 +0530

    Time: 203 ms (49.27%), Space: 14.9 MB (16.30%) - LeetHub

---
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -0,0 +1,45 @@
+class Solution:
+    def intervalIntersection(self, fl: List[List[int]], sl: List[List[int]]) -> List[List[int]]:
+        i,j,n,m,ans = 0,0,len(fl),len(sl),[]
+        def getOp(iv1,iv2):
+            print("overlap check",iv1,iv2,end="->")
+            # if iv1[0]<iv2[0]:
+            #     if iv1[1]>=iv2[0]:
+            #         # interval = [iv2[0],iv1[1]]
+            #         pass
+            #     else:
+            #         return False
+            # else:
+            #     if iv2[1]>=iv1[0]:
+            #         # interval = [iv1[0],iv2[1]]
+            #         pass
+            #     else:
+            #         return False
+            interval = [max(iv1[0],iv2[0]),min(iv1[1],iv2[1])]
+            # print(interval)
+            if interval[0]<=interval[1]:return interval
+            return False
+                
+        while i<n and j<m:
+            curI,curJ = fl[i],sl[j]
+            op = getOp(curI,curJ)
+            
+            if op!=False:
+                opTill = op[1]
+                ans.append(op)
+                if curI[1]==opTill:
+                    i+=1
+                if curJ[1]==opTill:
+                    j+=1
+            else:
+                nexI,nexJ = [inf,0],[inf,0]
+                if i<n-1:
+                    nexI = fl[i+1]
+                if j<m-1:
+                    nexJ = sl[j+1]
+                
+                if nexI[0]<nexJ[0]:
+                    i+=1
+                else:
+                    j+=1
+        return ans

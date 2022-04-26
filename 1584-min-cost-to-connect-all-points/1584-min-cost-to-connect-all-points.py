f8031019677feb54a401b828a2ead1cc9d2bf24e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-26 12:52:11 +0530

    Time: 1198 ms (91.30%), Space: 114.6 MB (39.92%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        visited = set()
+#         distance , vtx
+        distances = [inf for _ in points]
+        distances[0] = 0
+        parent = [-1 for _ in points]
+        pq = [(0,0)]
+        @cache
+        def getDist(i1,i2):
+            p1,p2 = points[i1],points[i2]
+            return abs(p1[0]-p2[0])+abs(p1[1]-p2[1])
+        while len(visited)<len(points):
+            dist,pIdx = heappop(pq)
+            if pIdx in visited:
+                continue
+            visited.add(pIdx)
+            point = points[pIdx]
+            
+            for i in range(len(points)):
+                if i not in visited:
+                    temp = getDist(pIdx,i) 
+                    if temp<distances[i]:
+                        distances[i] = temp
+                        heappush(pq,(temp,i))
+                        parent[i] = pIdx
+            # print("picked",points[minIdx],minDist)
+            # ans+=minDist
+        # print(distances)
+        # print(parent)
+        ans = 0
+        for i in range(len(points)):
+            if parent[i]!=-1:
+                ans+=getDist(i,parent[i])
+        return ans
+                        
+                    
+            
+        
+        

cea9ff031eaddc4e740a16fbb9cb629dd270892e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-27 11:31:40 +0530

    Time: 104 ms (46.51%), Space: 15.5 MB (20.47%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,4 +1,21 @@
+# class Solution:
+#     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+#         m = max(arr)
+#         return arr.index(m)
+
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        m = max(arr)
-        return arr.index(m)
+        n = len(arr)
+        l,r = 0,n-1
+        
+        while l<=r:
+            m = l+(r-l)//2
+            # print(l,r,m)
+            if arr[m-1]<arr[m]>arr[m+1]:
+                return m
+            if arr[m]<arr[m+1]:
+                l = m+1
+            if arr[m]>arr[m+1]:
+                r = m-1
+        # print("\n")
+    

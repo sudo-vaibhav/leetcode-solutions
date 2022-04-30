35cb5398e97d2fea34a6de18610e16c440edeb31
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-30 14:35:21 +0530

    Added solution - LeetHub

---
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+from heapq import heapify,heappop,heappush
+class Solution:
+    #Function to return the minimum cost of connecting the ropes.
+    def minCost(self,arr,n) :
+        cost = 0
+        heapify(arr)
+        
+        while len(arr)>1:
+            u,v = heappop(arr),heappop(arr)
+            cost += u+v
+            heappush(arr,u+v)
+        return cost
+        
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+import heapq
+from collections import  defaultdict
+# Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        n = int(input())
+        a = list(map(int, input().strip().split()))
+        ob=Solution()
+        print(ob.minCost(a,n))
+# } Driver Code Ends

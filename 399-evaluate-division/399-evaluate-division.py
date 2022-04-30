9a35ce2c8a6fc9ea8745cb115e25beeb79174392
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-04-30 14:21:12 +0530

    Time: 70 ms (5.77%), Space: 13.9 MB (95.43%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -3,27 +3,28 @@ class Solution:
         root = {}
         
         def find(x):
-            xp,xr = root.setdefault(x,(x,1.0))
-            if xp!=x:
-                grandparent,grandparentratio = find(xp)
-                root[x] = (grandparent,grandparentratio*xr)
+            parentX,ratioX = root.setdefault(x,(x,1.0))
+            if parentX!=x:
+                grandparent,grandparentratio = find(parentX)
+                root[x] = (grandparent,grandparentratio*ratioX)
             return root[x]
         
         def union(u,v,val):
-            pu,ru,pv,rv = *find(u),*find(v)
-            if pu!=ru:
-                root[pu]= (pv,val*rv/ru)
+            parentU,ratioU,parentV,ratioV = *find(u),*find(v)
+            if parentU!=parentV:
+                root[parentU]= (parentV,val*(ratioV/ratioU))
 
         @cache
         def getRatio(u,v):
             if u in root and v in root:
-                pu,ru,pv,rv = *find(u),*find(v)
-                if pu!=pv:
+                parentU,ratioU,parentV,ratioV = *find(u),*find(v)
+                if parentU!=parentV:
                     return -1.0
                 else:
-                    return ru/rv
+                    return ratioU/ratioV
             else:
                 return -1.0
+            
         for (u,v),val in zip(eqns,values): union(u,v,val)        
         return [getRatio(u,v) for u,v in qrys]
             

eaf951680303724117189a0b54a7997751630c29
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-03 23:35:55 +0530

    Time: 861 ms (20.52%), Space: 22.2 MB (68.21%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.py
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.py
@@ -1,34 +1,50 @@
+# class Solution:
+#     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
+#         n,ans = len(nums),0
+#         prevGreater = [-1 for _ in range(n)]
+#         stack = deque()
+
+#         for i in range(n):
+#             cur = nums[i]
+#             while len(stack)>0 and nums[stack[-1]]<cur:
+#                 stack.pop()
+            
+#             if len(stack)>0:
+#                 prevGreater[i] = stack[-1]
+#             stack.append(i)
+        
+        
+#         stack = deque()
+#         for j in range(n-1,-1,-1):
+#             cur = nums[j]
+#             temp = n
+#             while len(stack)>0 and nums[stack[-1]]<=cur:
+#                 stack.pop()
+            
+#             if len(stack)>0:
+#                 temp = stack[-1]
+                
+#             if left<=cur<=right:
+#                 ls = j - prevGreater[j]
+#                 rs = temp-j
+#                 ans+= ls*rs
+#             stack.append(j)
+            
+#         return ans
+
 class Solution:
     def numSubarrayBoundedMax(self, nums: List[int], left: int, right: int) -> int:
-        n,ans = len(nums),0
-        # nextGreater = [n for _ in range(n)]
-        prevGreater = [-1 for _ in range(n)]
-        stack = deque()
-
+        n,ans,prevInv,prevAns = len(nums),0,-1,0
         for i in range(n):
             cur = nums[i]
-            while len(stack)>0 and nums[stack[-1]]<cur:
-                stack.pop()
-            
-            if len(stack)>0:
-                prevGreater[i] = stack[-1]
-            stack.append(i)
-        
-        
-        stack = deque()
-        for j in range(n-1,-1,-1):
-            cur = nums[j]
-            temp = n
-            while len(stack)>0 and nums[stack[-1]]<=cur:
-                stack.pop()
-            
-            if len(stack)>0:
-                temp = stack[-1]
-                
+            if cur>right:
+                prevInv = i
+                prevAns = 0
+            if cur<left:
+                ans += prevAns
             if left<=cur<=right:
-                ls = j - prevGreater[j]
-                rs = temp-j
-                ans+= ls*rs
-            stack.append(j)
-            
+                prevAns = i-prevInv
+                ans += i-prevInv
+                
+        
         return ans

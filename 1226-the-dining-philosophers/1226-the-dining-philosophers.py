c683ac3b6bb1957191d468c112f239ae2e620a90
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-03 19:07:39 +0530

    Time: 181 ms (36.17%), Space: 15.2 MB (37.45%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.py
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.py
@@ -5,24 +5,16 @@ class DiningPhilosophers:
         self.eatingForks = [Condition() for i in range(5)]
         
     # call the functions directly to execute, for example, eat()
-    def wantsToEat(self,
-                   phil: int,
-                   pickLeftFork: 'Callable[[], None]',
-                   pickRightFork: 'Callable[[], None]',
-                   eat: 'Callable[[], None]',
-                   putLeftFork: 'Callable[[], None]',
-                   putRightFork: 'Callable[[], None]') -> None:
+    def wantsToEat(self,phil,pickLeftFork,pickRightFork,eat,putLeftFork,putRightFork):
         lower,higher = (phil)%5,(phil+1)%5
         if lower>higher:
             lower,higher = higher,lower
-            
         lf = self.eatingForks[lower]
         rf = self.eatingForks[higher]
         
-        with lf:
-            with rf:
-                pickLeftFork()
-                pickRightFork()
-                eat()
-                putLeftFork()
-                putRightFork()
+        with lf,rf:
+            pickLeftFork()
+            pickRightFork()
+            eat()
+            putLeftFork()
+            putRightFork()

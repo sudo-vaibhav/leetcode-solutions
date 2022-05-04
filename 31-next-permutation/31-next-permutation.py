ffdc90dce26db819e9344b47cb26ef3595d2b509
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-04 17:24:12 +0530

    Time: 78 ms (17.57%), Space: 13.9 MB (77.30%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,18 +3,14 @@ class Solution:
         n = len(a)
         for i in range(n-2,-1,-1):
             if a[i]<a[i+1]:
-                break
-        else:
-            a.sort()
-            return
-        # now i has the index where increasing part of permutation ends
-        # print(a[i])
-        for j in range(n-1,i,-1):
-            if a[j]>a[i]:
-                a[j],a[i] = a[i],a[j]
-                break
-        # print(a)
-        a[i+1:] = a[i+1:][::-1]
+                for j in range(n-1,i,-1):
+                    if a[j]>a[i]:
+                        a[j],a[i] = a[i],a[j]
+                        break
+                a[i+1:] = a[i+1:][::-1]
+                return
+        a.sort()
+        
         
         
         

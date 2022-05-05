3de82b13291c561c9d1b48191c6de5a4cc5cf1be
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-05 15:17:26 +0530

    Time: 569 ms (10.10%), Space: 24.3 MB (13.63%) - LeetHub

--- 398-random-pick-index/398-random-pick-index.py
+++ 398-random-pick-index/398-random-pick-index.py
@@ -8,10 +8,4 @@ class Solution:
     def pick(self, target: int) -> int:
         indices = self.pos[target]
         randIdx = randint(0,len(indices)-1)
-        # print(indices,randIdx)
         return indices[randIdx]
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.pick(target)

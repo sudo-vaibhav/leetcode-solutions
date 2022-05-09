96f0a049d59613bb2425340a65b43fff1cd73e94
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-09 16:32:51 +0530

    Time: 49 ms (48.39%), Space: 13.9 MB (79.05%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,26 +1,55 @@
+# KMP
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         def getLPS(s):
+#             n = len(s)
+#             lps = [0]*n
+#             i,Len = 1,0
+            
+#             while i<n:
+#                 if s[i]==s[Len]:
+#                     Len+=1
+#                     lps[i]=Len
+#                     i+=1
+#                 else:
+#                     if Len>0:
+#                         Len = lps[Len-1]
+#                     else:
+#                         lps[i]=0
+#                         i+=1
+#             return lps
+#         kmp = getLPS(needle+"#"+haystack)
+        
+#         for i in range(len(kmp)):
+#             if kmp[i]==len(needle):
+#                 return i-len(needle)*2
+#         return -1
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        def getLPS(s):
+        def getZArr(s):
             n = len(s)
-            lps = [0]*n
-            i,Len = 1,0
+            z = [0]*n
+            l,r=0,0
+            for i in range(1,n):
+#                 take headstart from known region
+                if i<=r:
+                    z[i] = min(r-i+1,z[i-l])
+#               get value from unknown region
+                while i+z[i]<n and s[i+z[i]]==s[z[i]]:
+                    z[i]+=1
             
-            while i<n:
-                if s[i]==s[Len]:
-                    Len+=1
-                    lps[i]=Len
-                    i+=1
-                else:
-                    if Len>0:
-                        Len = lps[Len-1]
-                    else:
-                        lps[i]=0
-                        i+=1
-            return lps
-        kmp = getLPS(needle+"#"+haystack)
+#             expand known region
+                if i+z[i]-1>r:
+                    l = i
+                    r = i+z[i]-1
+            return z
+        temp = needle+"#"+haystack
+        zArr = getZArr(temp)
+        print(temp)
+        print(zArr)
         
-        # print(kmp,len(needle))
-        for i in range(len(kmp)):
-            if kmp[i]==len(needle):
-                return i-len(needle)*2
+        for idx in range(len(zArr)):
+            if zArr[idx]==len(needle):
+                return idx-len(needle)-1
         return -1

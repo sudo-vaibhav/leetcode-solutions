0f221754d587f07d3f1de195010b2745b8eb2d16
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-10 13:25:39 +0530

    Time: 31 ms (87.25%), Space: 13.9 MB (69.47%) - LeetHub

---
+++ 50-powx-n/50-powx-n.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n<0:
+            x = 1/x
+            n *= -1
+        def powerRecursive(x,n):
+            if n==0:
+                return 1
+            if n==1:
+                return x
+            if n%2==1:
+                return x*powerRecursive(x,n-1)
+            else:
+                return powerRecursive(x,n//2)**2
+        
+        return powerRecursive(x,n)
+        

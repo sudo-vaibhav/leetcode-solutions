515638df3a0694c65f238995fc1c527d79e11695
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-11 10:25:42 +0530

    Time: 181 ms (5.35%), Space: 14.7 MB (18.38%) - LeetHub

---
+++ 343-integer-break/343-integer-break.py
@@ -0,0 +1,13 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        # ans = n-1
+        
+        @cache
+        def solve(n,cnt):
+            if n<=1:
+                return 1
+            tempans = -inf
+            for i in range(1,n+(1 if cnt>0 else 0)):
+                tempans = max(tempans,solve(n-i,cnt+1)*i)
+            return tempans
+        return solve(n,0)

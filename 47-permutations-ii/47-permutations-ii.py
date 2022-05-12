a78d655597ff2bbaef3e605c0b2ae04fb91167f9
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-12 09:49:19 +0530

    Time: 58 ms (92.68%), Space: 14.5 MB (10.49%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
+        ans = []
         n = len(nums)
         def solve(idx):
             if idx==n:
-                ans.add(tuple(nums))
+                ans.append(tuple(nums))
             else:
+                done = set()
                 for i in range(idx,n):
-                    nums[idx],nums[i] = nums[i],nums[idx]
-                    solve(idx+1)
-                    nums[idx],nums[i] = nums[i],nums[idx]
+                    if nums[i] not in done:
+                        nums[idx],nums[i] = nums[i],nums[idx]
+                        solve(idx+1)
+                        nums[idx],nums[i] = nums[i],nums[idx]
+                        done.add(nums[i])
         solve(0)
         return ans

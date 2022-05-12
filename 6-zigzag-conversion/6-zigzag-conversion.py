6d412684091689e4d0e221f811a84cac11ea134a
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-12 10:36:25 +0530

    Time: 130 ms (18.36%), Space: 14.1 MB (50.56%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -1,24 +1,21 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1: return s
-        x,y = 0,0
+        y = 0
         LEN = len(s)
-        mat = [["" for _ in range(1000)] for _ in range(numRows)]
-        m,n = len(mat),len(mat[0])
-        down = True
+        mat = ["" for _ in range(numRows)]
+        # m,n = len(mat),len(mat[0])
+        down = False
+        ans = ""
         for i in range(len(s)):
-            # print(x,y)
-            mat[y][x] = s[i]
-            if y==numRows-1:
-                down=False
-            elif y==0:
-                down =True
+            mat[y] += s[i]
+            if y==numRows-1 or y==0:
+                down=not down
             if down:
                 y+=1
             else:
                 y-=1
-                x+=1
-        ans = ""
+                    
         for row in mat:
-            ans+= "".join(row)
+            ans+= row
         return ans

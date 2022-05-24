dd77eb8f658534b5f7385107b6a611a93727c1f3
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-24 10:35:23 +0530

    Time: 30 ms (88.96%), Space: 14.8 MB (11.69%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,34 +1,34 @@
 class Solution(object):
     def longestValidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
         stack = deque()
-        invalids = []
+        validSubstringDelimiters = []
         ans = 0
-        
+
         for idx,char in enumerate(s):
             if char==")":
-                if stack:
+                if stack: # if it already has some opening parenthesis
                     stack.pop()
                 else:
-                    invalids.append(idx)
+                    # else any valid substring wont have current char as a part (since this char will never be matched by anything)
+                    validSubstringDelimiters.append(idx)
             else:
+                # always add an opening parenthesis to stack
                 stack.append(idx)
-                
+
+        # any unpopped opening parenthesis can also not be part of valid substring
         while stack:
-            invalids.append(stack.pop())
+            validSubstringDelimiters.append(stack.pop())
         
-        invalids.extend([-1,len(s)])
-        invalids.sort()
-        for i in range(1,len(invalids)):
-            prev = invalids[i-1]
-            cur  = invalids[i]
-            
-            ans = max(ans,cur-prev-1)
-            
-        # print(invalids)
-            
+        # -1 and n are also valid delimiters and we need to account for these corner cases
+        validSubstringDelimiters.extend([-1,len(s)])
+
+        # sorting needed as two nearest invalid indices will have valid
+        # substring in between them, only adjacent pairs need to be 
+        # considered as non-adjacents will have an invalid character between them
+        validSubstringDelimiters.sort()
+
+        for i in range(1,len(validSubstringDelimiters)):
+            prev = validSubstringDelimiters[i-1]
+            cur  = validSubstringDelimiters[i]            
+            ans = max(ans,cur-prev-1) # consider current substring length and store max possible val
         return ans
-        

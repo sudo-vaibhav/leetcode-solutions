3a9b5148d7c11af8a03b4a1f368d04c4429d115e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-26 15:07:39 +0530

    Time: 12411 ms (8.92%), Space: 31.6 MB (5.01%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        N = len(nums)
+        
+        @cache
+        def solve(idx):
+            if idx>=N: return inf
+            if idx==N-1:
+                return 0
+            val = nums[idx]
+            ans = inf
+            for jumps in range(1,val+1):
+                ans = min(ans,1+solve(idx+jumps))
+            return ans
+        return solve(0)

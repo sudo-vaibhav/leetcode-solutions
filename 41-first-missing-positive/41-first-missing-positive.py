26860bcaf74a75000e4baeff7cdd5274957be907
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-26 15:18:59 +0530

    Time: 1311 ms (32.24%), Space: 63.3 MB (42.36%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.py
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -1,17 +1,16 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-#         thumb rule, positive number j should be present at index j-1
-        i,n = 0,len(nums)
-        
-        while i<n:
-            if nums[i]>0 and nums[i]<=n and nums[nums[i]-1]!=nums[i]:
-                temp = nums[nums[i]-1]
-                cur = nums[i]
-                nums[i],nums[cur-1] = temp,cur
-            else:
-                i+=1
-        for i in range(0,n):
+        n = len(nums)
+        for i in range(n):
+            while True:
+                val = nums[i]
+                if val<=0 or val>n or val==i+1:
+                    break
+                else:
+                    if nums[val-1]==nums[i]:break
+                    nums[val-1],nums[i] = nums[i],nums[val-1]
+        for i in range(n):
             if nums[i]!=i+1:
                 return i+1
-        
+                
         return n+1

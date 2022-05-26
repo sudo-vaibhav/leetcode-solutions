7a17627cd072233fa951d43e19057c6d462ae0a0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-26 15:38:50 +0530

    Time: 32 ms (94.24%), Space: 14 MB (31.52%) - LeetHub

---
+++ 765-couples-holding-hands/765-couples-holding-hands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minSwapsCouples(self, row: List[int]) -> int:
+        n = len(row)
+        cnt = 0
+        for i in range(0,n,2):
+            v1,v2 = row[i],row[i+1]
+            couple = True
+            if v1%2==v2%2: couple = False
+            else:
+                oddIdx = i if v1%2==1 else i+1
+                evenIdx = i if oddIdx!=i else i+1
+                oddPair = row[oddIdx]-1
+                
+                if oddPair!=row[evenIdx]:
+                    couple=False
+            if couple:
+                continue
+            else:
+                if v1%2==1:partner=v1-1
+                else:partner=v1+1
+                for j in range(i+2,n):
+                    if row[j]==partner:
+                        row[i+1],row[j] = row[j],row[i+1]
+                        break
+                cnt+=1
+                
+                
+                
+        return cnt

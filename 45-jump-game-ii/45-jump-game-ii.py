301fd52fe1640c0fde97091273295543afbdefc9
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-26 15:08:29 +0530

    Time: 11935 ms (9.88%), Space: 32.7 MB (5.01%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,9 +4,9 @@ class Solution:
         
         @cache
         def solve(idx):
-            if idx>=N: return inf
-            if idx==N-1:
-                return 0
+            if idx>=N-1: return 0
+            # if idx==N-1:
+            #     return 0
             val = nums[idx]
             ans = inf
             for jumps in range(1,val+1):

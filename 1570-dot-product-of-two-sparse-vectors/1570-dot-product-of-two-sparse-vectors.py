bd04d1cd57cebd579fb6b73bf89f42309883e842
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-28 17:03:44 +0530

    Time: 2374 ms (46.67%), Space: 19 MB (16.06%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.py
@@ -1,25 +1,42 @@
+# class SparseVector:
+#     def __init__(self, nums: List[int]):
+#         self.mapping = {}
+#         for idx in range(len(nums)):
+#             self.mapping[idx] = nums[idx]
+        
+
+#     # Return the dotProduct of two sparse vectors
+#     def dotProduct(self, vec: 'SparseVector') -> int:
+#         ans = 0
+#         if len(self.mapping)<len(vec.mapping):
+#             lessKeys = self.mapping
+#             other = vec.mapping
+#         else:
+#             lessKeys = vec.mapping
+#             other = self.mapping
+#         for idx in lessKeys:
+#             if idx in other:
+#                 ans += lessKeys[idx]*other[idx]
+#         return ans
+
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.mapping = {}
+        self.arr = []
         for idx in range(len(nums)):
-            self.mapping[idx] = nums[idx]
+            if nums[idx]!=0:
+                self.arr.append((idx,nums[idx]))
         
 
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
         ans = 0
-        if len(self.mapping)<len(vec.mapping):
-            lessKeys = self.mapping
-            other = vec.mapping
-        else:
-            lessKeys = vec.mapping
-            other = self.mapping
-        for idx in lessKeys:
-            if idx in other:
-                ans += lessKeys[idx]*other[idx]
+        i,j = 0,0
+        while i<len(self.arr) and j<len(vec.arr):
+            if self.arr[i][0]==vec.arr[j][0]:
+                ans+=self.arr[i][1]*vec.arr[j][1]
+                i,j = i+1,j+1
+            elif self.arr[i][0]<vec.arr[j][0]:
+                i+=1
+            else:
+                j+=1
         return ans
-
-# Your SparseVector object will be instantiated and called as such:
-# v1 = SparseVector(nums1)
-# v2 = SparseVector(nums2)
-# ans = v1.dotProduct(v2)

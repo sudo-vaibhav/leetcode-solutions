a01d8cacfb8991673a385e24f50e62266e0cb9bc
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-28 23:11:27 +0530

    Time: 170 ms (22.50%), Space: 21.8 MB (60.13%) - LeetHub

---
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.py
@@ -0,0 +1,28 @@
+class WordDistance:
+
+    def __init__(self, W: List[str]):
+        self.mapping = defaultdict(list)
+        for i, w in enumerate(W):
+            self.mapping[w].append(i)
+        
+    
+    # @cache
+    def shortest(self, word1: str, word2: str) -> int:
+        def findClosest(v,arr):
+            if v<arr[0]:
+                return arr[0]-v
+            elif v>arr[-1]:
+                return v-arr[-1]
+            else:
+                i = bisect_left(arr,v)
+                return min(abs(arr[i]-v),abs(arr[i-1]-v))
+            
+        ans = inf
+        for option in self.mapping[word1]:
+            ans = min(ans,findClosest(option,self.mapping[word2]))
+        return ans
+
+
+# Your WordDistance object will be instantiated and called as such:
+# obj = WordDistance(wordsDict)
+# param_1 = obj.shortest(word1,word2)

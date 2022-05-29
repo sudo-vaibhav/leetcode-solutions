f2e17b9f903d54fd180873a59fa9c690655bff0c
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-29 17:58:29 +0530

    Time: 191 ms (5.03%), Space: 18.1 MB (6.91%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,16 +1,28 @@
+# class Solution:
+#     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+#         rooms = []
+#         ans = 0
+#         end,start = 0,1
+#         intervals.sort()
+#         for interval in intervals:
+#             if len(rooms)==0 or rooms[0][end]>interval[0]:
+#                 pass
+#             else:
+#                 heappop(rooms)
+#             heappush(rooms,(interval[1],interval[0]))
+                
+#             ans = max(ans,len(rooms))
+#         return ans
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        rooms = []
-        ans = 0
-        end,start = 0,1
-        intervals.sort()
-        for interval in intervals:
-            if len(rooms)==0 or rooms[0][end]>interval[0]:
-                pass
+        times = sorted([(x[0],"start") for x in intervals]+[(x[1],"end") for x in intervals])
+        cur,ans= 0,0
+        for time,timeType in times:
+            if timeType=="start":
+                cur+=1
             else:
-                heappop(rooms)
-            heappush(rooms,(interval[1],interval[0]))
-                
-            ans = max(ans,len(rooms))
-        
+                cur-=1
+            ans = max(ans,cur)
         return ans
+                

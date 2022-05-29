1dc623cbef95e30958ca6238aa81819f7219ec1a
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-29 17:59:35 +0530

    Time: 130 ms (27.15%), Space: 17.6 MB (49.72%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,28 +1,29 @@
-# class Solution:
-#     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-#         rooms = []
-#         ans = 0
-#         end,start = 0,1
-#         intervals.sort()
-#         for interval in intervals:
-#             if len(rooms)==0 or rooms[0][end]>interval[0]:
-#                 pass
-#             else:
-#                 heappop(rooms)
-#             heappush(rooms,(interval[1],interval[0]))
-                
-#             ans = max(ans,len(rooms))
-#         return ans
-
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        times = sorted([(x[0],"start") for x in intervals]+[(x[1],"end") for x in intervals])
-        cur,ans= 0,0
-        for time,timeType in times:
-            if timeType=="start":
-                cur+=1
+        rooms = []
+        ans = 0
+        end,start = 0,1
+        intervals.sort()
+        for interval in intervals:
+            if len(rooms)==0 or rooms[0][end]>interval[0]:
+                pass
             else:
-                cur-=1
-            ans = max(ans,cur)
+                heappop(rooms)
+            heappush(rooms,(interval[1],interval[0]))
+                
+            ans = max(ans,len(rooms))
         return ans
+
+
+# class Solution:
+#     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+#         times = sorted([(x[0],"start") for x in intervals]+[(x[1],"end") for x in intervals])
+#         cur,ans= 0,0
+#         for time,timeType in times:
+#             if timeType=="start":
+#                 cur+=1
+#             else:
+#                 cur-=1 # end gets removed before due to end being lexicographically smaller than start
+#             ans = max(ans,cur)
+#         return ans
                 

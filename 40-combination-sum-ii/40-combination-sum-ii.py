8a4d73abea9c330ebc7aa854c4431701807fe1a9
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-30 13:10:40 +0530

    Time: 150 ms (21.71%), Space: 14 MB (57.93%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,48 +1,20 @@
 class Solution(object):
     def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        
-#       set of candidates
-#       target
-#       hint for recursion - combination, RETURN ALL
-#       constraints - will be very less, especially when generate all is there
-
-        # unlimited number of times : unbounded knapsack
-        ans = []
         ctr = Counter(candidates)
-        nums = list(ctr.keys())
-        N = len(nums)
-#       generates and returns all combinations of the subarray from idx-> which sum to target
-        def solve(idx,target):
-            if idx>=N:
-                if target==0:
-                    return [[]]
-                else:
-                    return []
-            else:
-                cur = nums[idx]
-                
-#                 knapsack variation, i can choose to pick or not pick
-                tempans = []
-                
-#               not pick i can always do
-                for pickAmt in range(0,ctr[cur]+1):
-                    deductedAmt = pickAmt*cur
-                    
-                    if deductedAmt<=target:
-                        combs = solve(idx+1,target-deductedAmt)
-                        for seq in combs:
-                            tempans.append([cur]*pickAmt+seq)
-                return tempans
-#                 both pick and not pick are arrays
-            
+        uniques = list(ctr.keys())
+        N,ans = len(uniques),[]
         
-        return solve(0,target)
-    
+        def solve(idx,target,path=[]):
+            if idx==N:
+                if target==0:
+                    ans.append(list(path))
+            else:
+                cur = uniques[idx]
+                for pickCount in range(0,ctr[cur]+1):
+                    if pickCount*cur<=target:
+                        solve(idx+1,target-pickCount*cur,path+[cur]*pickCount)
+        solve(0,target,[])
+        return ans
     
         # iterative subsets
         

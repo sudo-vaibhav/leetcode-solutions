04d4f75bc9235875f9fa3cbfa6b0811963e216d4
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-30 13:25:29 +0530

    Time: 70 ms (80.90%), Space: 13.9 MB (57.93%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,26 +1,34 @@
 class Solution(object):
     def combinationSum2(self, candidates, target):
-        ctr = Counter(candidates)
-        uniques = list(ctr.keys())
-        N,ans = len(uniques),[]
-        
-        def solve(idx,target,path=[]):
-            if idx==N:
-                if target==0:
-                    ans.append(list(path))
+        # ctr = Counter(candidates)
+        # uniques = list(ctr.keys())
+        # N,ans = len(uniques),[]
+        # def solve(idx,target,path=[]):
+        #     if idx==N:
+        #         if target==0:
+        #             ans.append(list(path))
+        #     else:
+        #         cur = uniques[idx]
+        #         for pickCount in range(0,ctr[cur]+1):
+        #             if pickCount*cur<=target:
+        #                 solve(idx+1,target-pickCount*cur,path+[cur]*pickCount)
+        # solve(0,target,[])
+        # return ans
+        candidates.sort()
+        N = len(candidates)
+        ans = []
+        def solve(idx,target,path):
+            # if idx==N:
+            if target==0:
+                ans.append(list(path))
             else:
-                cur = uniques[idx]
-                for pickCount in range(0,ctr[cur]+1):
-                    if pickCount*cur<=target:
-                        solve(idx+1,target-pickCount*cur,path+[cur]*pickCount)
+                for i in range(idx,N):
+                    if i>idx and candidates[i]==candidates[i-1]:continue
+                    if candidates[i]>target:break
+                    path.append(candidates[i])
+                    solve(i+1,target-candidates[i],path)
+                    path.pop()
+                    
+                    
         solve(0,target,[])
         return ans
-    
-        # iterative subsets
-        
-        # [1,2,3,4]
-#         generate all subsets
-#         res = {[]}
-#         res = {[],[1]}
-#         res = {[],[1],[3],[1,3]}
-

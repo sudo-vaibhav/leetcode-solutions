79d26c28d41847aa02dd8c355764c15f3f853e14
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-05-30 14:15:35 +0530

    Time: 30 ms (94.02%), Space: 13.8 MB (98.68%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.py
+++ 60-permutation-sequence/60-permutation-sequence.py
@@ -2,19 +2,13 @@ class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         nums = list(range(1,n+1))
         ans = []
-        def solve(k):
+        while len(nums)>0:
             N = len(nums)
-            if N==0:return
             totalPerms = factorial(N)
             perHead = totalPerms//N
-            
             numPickedIdx = (k-1)//perHead
             pickedNum = nums[numPickedIdx]
             ans.append(str(pickedNum))
             nums.remove(pickedNum)
-            newK = k%perHead
-            solve(newK)
-            
-            
-        solve(k)
+            k = k%perHead
         return "".join(ans)

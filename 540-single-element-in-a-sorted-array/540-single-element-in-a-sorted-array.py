2db814db6b20ae5c899e236f2f48185f702dccb4
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-01 12:29:19 +0530

    Time: 290 ms (22.45%), Space: 23.7 MB (45.01%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,30 +1,27 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        l,r = 0,len(nums)-2
+        l,r = 0,len(nums)-1
 #         if all were appearing twice, first occurence of num occurs in even position
 #         and second occurence occurs in odd position
         while l<=r:
             mid = l+(r-l)//2
             cur = nums[mid]
-            if nums[mid]==nums[mid^1]:
-                l = mid+1
+            
+            if mid>0 and nums[mid-1]==cur:
+#                 this is second occurence
+                if mid%2==0:
+#         outlier lies behind
+                    r = mid-1
+                else:
+                    l = mid+1
+            elif mid<len(nums)-1 and nums[mid+1]==cur:
+                if mid%2==1:
+                    r = mid-1
+                else:
+                    l = mid+1
             else:
-                r = mid-1
-#             if mid>0 and nums[mid-1]==cur:
-# #                 this is second occurence
-#                 if mid%2==0:
-# #         outlier lies behind
-#                     r = mid-1
-#                 else:
-#                     l = mid+1
-#             elif mid<len(nums)-1 and nums[mid+1]==cur:
-#                 if mid%2==1:
-#                     r = mid-1
-#                 else:
-#                     l = mid+1
-            # else:
-                # return nums[mid]
+                return nums[mid]
             
             
         
-        return nums[l]
+#         return nums[l]

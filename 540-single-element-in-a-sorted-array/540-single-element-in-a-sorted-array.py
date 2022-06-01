a2e28e8b769a54ac734423de2b108e44d23559da
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-01 12:20:43 +0530

    Time: 267 ms (31.81%), Space: 23.7 MB (45.01%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,27 +1,29 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        l,r = 0,len(nums)-1
+        l,r = 0,len(nums)-2
 #         if all were appearing twice, first occurence of num occurs in even position
 #         and second occurence occurs in odd position
-        # if len(nums)==1:return nums[0]
         while l<=r:
             mid = l+(r-l)//2
             cur = nums[mid]
-            if mid>0 and nums[mid-1]==cur:
-#                 this is second occurence
-                if mid%2==0:
-#         outlier lies behind
-                    r = mid-1
-                else:
-                    l = mid+1
-            elif mid<len(nums)-1 and nums[mid+1]==cur:
-                
-                if mid%2==1:
-                    r = mid-1
-                else:
-                    l = mid+1
+            if nums[mid]==nums[mid^1]:
+                l = mid+1
             else:
-                return nums[mid]
+                r = mid-1
+#             if mid>0 and nums[mid-1]==cur:
+# #                 this is second occurence
+#                 if mid%2==0:
+# #         outlier lies behind
+#                     r = mid-1
+#                 else:
+#                     l = mid+1
+#             elif mid<len(nums)-1 and nums[mid+1]==cur:
+#                 if mid%2==1:
+#                     r = mid-1
+#                 else:
+#                     l = mid+1
+            # else:
+                # return nums[mid]
             
             
         

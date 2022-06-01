af83c4b8db9fc69a9af76a98374de63947eab0e0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-01 12:00:24 +0530

    Time: 271 ms (29.98%), Space: 23.9 MB (12.71%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,22 +1,30 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        l,n = 0,len(nums)
-        r = n-1
-        if n==1: return nums[0]
-        def getTwiceUpperBound(idx):
-            if idx<n-1 and nums[idx+1]==nums[idx]:
-                return idx+1
-            if idx>0 and nums[idx-1]==nums[idx]:
-                return idx
-            return -1
+        l,r = 0,len(nums)-1
+#         if all were appearing twice, first occurence of num occurs in even position
+#         and second occurence occurs in odd position
+        if len(nums)==1:return nums[0]
+    
         while l<=r:
-            m = l+(r-l)//2
-            temp = getTwiceUpperBound(m)
-            if temp == -1:
-                return nums[m]
-            if temp%2==1:
-                l = m+1
+            mid = l+(r-l)//2
+            cur = nums[mid]
+            
+            if mid>0 and nums[mid-1]==cur:
+#                 this is second occurence
+                if mid%2==0:
+#         outlier lies behind
+                    r = mid-1
+                else:
+                    l = mid+1
+            elif mid<len(nums)-1 and nums[mid+1]==cur:
+                
+                if mid%2==1:
+                    r = mid-1
+                else:
+                    l = mid+1
             else:
-                r = m-1
-        return -1
+                return nums[mid]
+            
+            
         
+        return nums[l]

b9af03475a527c8d54fe6dfe1bcda56f3a826713
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-07 18:17:36 +0530

    Time: 411 ms (75.28%), Space: 14.5 MB (21.40%) - LeetHub

---
+++ 839-similar-string-groups/839-similar-string-groups.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numSimilarGroups(self, strs: List[str]) -> int:
+        strs = [list(s) for s in strs]
+        
+        class UF:
+            def __init__(self,n):
+                self.parent = [i for i in range(n)]
+                self.sz = n
+            
+            def union(self,u,v):
+                pu,pv = self.find(u),self.find(v)
+                if pu==pv:
+                    return False
+                else:
+                    self.parent[pu]=pv
+                    self.sz-=1
+                    return True
+            
+            def find(self,v):
+                if self.parent[v]==v:
+                    return v
+                else:
+                    self.parent[v] = self.find(self.parent[v])
+                    return self.parent[v]
+            
+        N = len(strs)
+        uf = UF(N)
+        
+        def comp(s1,s2):
+            n = 0
+            for i in range(len(s1)):
+                if s1[i]!=s2[i]:
+                    if n==2:
+                        return False
+                    n+=1
+            return True
+        
+        for i in range(N):
+            for j in range(i+1,N):
+                if comp(list(strs[i]),list(strs[j])):
+                    uf.union(i,j)
+        return uf.sz

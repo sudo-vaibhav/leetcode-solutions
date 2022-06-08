1b1ec8f35a306369febfe8be4fa6ea664a00c78e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-08 12:00:03 +0530

    Time: 74 ms (57.86%), Space: 14.1 MB (95.13%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        
+        grtr = defaultdict(lambda : -1)
+        st = []
+        for num in nums2:
+            if len(st)==0 or st[-1]>num:
+                st.append(num)
+            else:
+                while len(st)!=0 and st[-1]<num:
+                    grtr[st[-1]] = num
+                    st.pop()
+                st.append(num)
+        
+        return [grtr[num] for num in nums1]
+                

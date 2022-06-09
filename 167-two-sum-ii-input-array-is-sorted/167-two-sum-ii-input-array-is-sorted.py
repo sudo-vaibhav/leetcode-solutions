bfe1bfc374263e67373057755b8ff812ad725ae8
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-09 10:28:31 +0530

    Time: 218 ms (28.46%), Space: 14.9 MB (41.52%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,11 +1,23 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(n):
+#             cur = nums[i]
+#             remain = target-cur
+#             f = bisect_left(nums,remain,i+1)
+#             if f<n and nums[f]==remain:
+#                 return [i+1,f+1]
+        
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        for i in range(n):
-            cur = nums[i]
-            remain = target-cur
-            f = bisect_left(nums,remain,i+1)
-            
-            if f<n and nums[f]==remain:
-                return [i+1,f+1]
-        
+        i,j = 0,n-1
+        while i<j:
+            X,Y = nums[i],nums[j]
+            tot = X+Y
+            if tot>target:
+                j-=1
+            elif tot<target:
+                i+=1
+            else:
+                return [i+1,j+1]

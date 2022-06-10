deda1476a4670c0d7e25ab1fa908b320f57fc4da
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-10 17:47:31 +0530

    Time: 581 ms (42.68%), Space: 19.3 MB (95.07%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.py
+++ 901-online-stock-span/901-online-stock-span.py
@@ -1,7 +1,7 @@
 class StockSpanner:
 
     def __init__(self):
-        self.st = []        
+        self.st = deque()       
         self.cnt = 1
     def next(self, price: int) -> int:
         while self.st and self.st[-1][0]<=price:
@@ -11,8 +11,3 @@ class StockSpanner:
         self.st.append((price,self.cnt))
         self.cnt+=1
         return ans
-
-
-# Your StockSpanner object will be instantiated and called as such:
-# obj = StockSpanner()
-# param_1 = obj.next(price)

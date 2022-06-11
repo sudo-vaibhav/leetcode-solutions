ac98ed1afced5db191a30c4cbceec56c8aaaf9e0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-11 17:00:47 +0530

    Time: 412 ms (71.65%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -1,13 +1,3 @@
-# Write your MySQL query statement below
-# WITH A as (Select id as curID, count(*) as children from Tree having p_id=curID group by curID)
-# select * from A
-# Tree.id,
-# CASE
-#     WHEN p_id is NULL then "Root"
-#     WHEN A.children=0 then "Leaf"
-#     ELSE "Inner"
-# END AS type from Tree inner join A on A.id=Tree.id 
-
 select id, case when 
 p_id is null then "Root"
 when id in (select p_id from Tree) then "Inner"

21c74a540c7b2670836e512cef9b2ff068a7c51d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-13 12:41:53 +0530

    Time: 55 ms (38.38%), Space: 14 MB (29.21%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,44 @@
+start,processing,end = 0,1,2
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        class Automaton:
+            def __init__(self):
+                self.state = start
+                self.mult = 1
+                self.val = 0
+            
+            def process(self,c):
+                if self.state==start:
+                #                         keep ignoring whitespaces
+                    if c==" ":
+                        return
+                    else:
+                        self.state = processing # start phase over
+                        if c=="+" or c=="-":
+                            if c=="-":
+                                self.mult=-1    
+                        else:
+                    #                           nums should start now, so process it again to count that num without writing
+                    #                           more logic in start phase
+                            self.process(c)
+                elif self.state==processing:
+                    if c.isdigit():
+                        temp = self.val*10+int(c)
+                        self.val = min(2**31,temp)
+                        # self.val = max(-2**31,self.val)
+                    else:
+                    #                         mark end of phase
+                        self.state=end
+                        
+            def verdict(self):
+                base = self.val
+                if base == 2**31 and self.mult==1:
+                    base-=1
+                return self.mult*base
+    
+        automaton = Automaton()
+        
+        for c in s:
+            automaton.process(c)
+        
+        return automaton.verdict()

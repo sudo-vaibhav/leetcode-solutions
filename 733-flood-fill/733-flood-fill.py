faa593bede6a2bc47700838d19de8bbe6f4dec03
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-13 21:28:49 +0530

    Time: 153 ms (8.82%), Space: 14.2 MB (37.18%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,22 +1,14 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        q = collections.deque()
-        q.append((sr,sc))
-        orig = image[sr][sc]
-        image[sr][sc] = newColor
         m,n = len(image),len(image[0])
-        done = set()
-        while q:
-            r,c = q.popleft()
-            for dr,dc in [(-1,0),(1,0),(0,-1),(0,1)]:
-                R,C = r+dr,c+dc
-                # print(R,m,C,n)
-                if (R>=0 and R<m and C>=0 and C<n):
-                    if image[R][C] == orig:
-                        image[R][C] = newColor
-                        if (R,C) not in done:
-                            q.append((R,C))
-                            done.add((R,C))
-                
-                
+        baseColor = image[sr][sc]
+        if baseColor!=newColor:
+            moves = [(-1,0),(1,0),(0,1),(0,-1)]
+            def dfs(r,c):
+                if 0<=r<m and 0<=c<n and image[r][c]==baseColor:
+                    image[r][c]=newColor
+                    for dr,dc in moves:
+                        R,C = r+dr,c+dc
+                        dfs(R,C)
+            dfs(sr,sc)
         return image

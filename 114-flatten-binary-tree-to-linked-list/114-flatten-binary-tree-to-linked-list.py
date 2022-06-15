a4079db03531d10b4ba7f7e17f2cda80e196eb66
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-15 15:20:13 +0530

    Time: 68 ms (20.06%), Space: 15.3 MB (47.25%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -1,16 +1,30 @@
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
-        prev = None
+#         prev = None
         
-        def vlr(node):
-            nonlocal prev
-            if not node: return
-            L,R = node.left,node.right
-            if prev:
-                prev.right = node
-            prev = node
-            node.left = None
-            vlr(L)
-            vlr(R)
+#         def vlr(node):
+#             nonlocal prev
+#             if not node: return
+#             L,R = node.left,node.right
+#             if prev:
+#                 prev.right = node
+#             prev = node
+#             node.left = None
+#             vlr(L)
+#             vlr(R)
+#         vlr(root)
 
-        vlr(root)
+        # prev= None
+        temp = root
+        
+        while temp:
+            if temp.left:
+                rightMost = temp.left
+                while rightMost.right:
+                    rightMost = rightMost.right
+                
+                rightMost.right = temp.right
+                temp.right = temp.left
+                temp.left = None
+            temp = temp.right
+        

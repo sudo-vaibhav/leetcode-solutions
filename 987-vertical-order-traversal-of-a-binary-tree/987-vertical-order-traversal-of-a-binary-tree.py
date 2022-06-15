1c94eaa911ec27702e60f66369228bfe75a2bd91
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-15 12:21:09 +0530

    Time: 69 ms (9.12%), Space: 14.6 MB (25.20%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -8,17 +8,15 @@ from sortedcontainers import SortedDict,SortedList
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         colMap = SortedDict({})
-        
         def vlr(node,row=0,col=0):
             if not node: return
-            else:
-                if not col in colMap:
-                    colMap[col]=SortedDict({})
-                if row not in colMap[col]:
-                    colMap[col][row] = SortedList()
-                colMap[col][row].add(node.val)
-                vlr(node.left,row+1,col-1)
-                vlr(node.right,row+1,col+1)
+            if not col in colMap:
+                colMap[col]=SortedDict({})
+            if row not in colMap[col]:
+                colMap[col][row] = SortedList()
+            colMap[col][row].add(node.val)
+            vlr(node.left,row+1,col-1)
+            vlr(node.right,row+1,col+1)
         vlr(root)
         ans = []
         for col in colMap:

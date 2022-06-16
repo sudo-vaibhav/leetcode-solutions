d4a98776df78a3d10ad87426426bbc53db907e5e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-16 12:55:58 +0530

    Time: 96 ms (56.57%), Space: 26.2 MB (60.89%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,20 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        @cache
-        def isIn(root,val):
-            if not root : return False
-            return root==val or isIn(root.left,val) or isIn(root.right,val)
-        def solve(root,p,q):
-            # print(root.val)
-            if root == p or root== q : return root
-            lp = isIn(root.left,p)
-            lq = isIn(root.left,q)
-            rp = isIn(root.right,p)
-            rq = isIn(root.right,q)
-            if((lp and rq)or(lq and rp)): return root
-            else:
-                if lp and lq: return solve(root.left,p,q)
-                else: return solve(root.right,p,q)
         
-        return solve(root,p,q)
+        
+        def lcaOrFind(root,p,q):
+            if root==None or root==p or root==q:
+                return root
+            L = lcaOrFind(root.left,p,q)
+            R = lcaOrFind(root.right,p,q)
+            
+            if L and R:
+                return root
+            else:
+                return L or R
+            
+        
+        return lcaOrFind(root,p,q)

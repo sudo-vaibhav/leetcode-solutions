3c0363f872c651cfcd4f55c6738f35e1f3847c80
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-16 14:36:35 +0530

    Time: 53 ms (74.99%), Space: 16.3 MB (85.61%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.py
@@ -1,37 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
         def isLeaf(node):
             return node and not(node.right or node.left)
-        
         def leftBound(node):
             res = []
             while node and not isLeaf(node):
                 res.append(node.val)
                 node = node.left or node.right
             return res
-        
         def rightBound(node):
             res = []
             while node and not isLeaf(node):
                 res.append(node.val)
                 node = node.right or node.left
             return res
-            
-        
         def getLeaves(node,leaves):
             if not node: return 
             if isLeaf(node):
                 leaves.append(node.val)
             getLeaves(node.left,leaves)
             getLeaves(node.right,leaves)
-                
-        
         ans = [root.val]
         if isLeaf(root): return ans
         else:
@@ -39,11 +27,5 @@ class Solution:
             leaves = []
             getLeaves(root,leaves)
             ans.extend(leaves)
-            
             ans.extend(rightBound(root.right)[::-1],)
             return ans
-            
-        
-        
-        
-        

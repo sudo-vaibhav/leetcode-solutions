b3c961b7542d15ac8ae3b9b1852b12bdfb9682c8
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-17 12:54:56 +0530

    Time: 123 ms (5.07%), Space: 17.1 MB (5.36%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.py
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -8,9 +8,15 @@ class Solution:
                 if monitoredThroughCur:
                     return solve(node.left,True,False)+solve(node.right,True,False)
                 else:
-                    return min(solve(node.left,False,NotApplicable)+solve(node.right,False,NotApplicable)
-                               , 1+solve(node.left,True,False)+solve(node.right,True,False))
+                    return min(
+                        solve(node.left,False,NotApplicable)+solve(node.right,False,NotApplicable), 
+                        1+solve(node.left,True,False)+solve(node.right,True,False)
+                    )
             else:
-                return 1+min(solve(node.left,True,True)+solve(node.right,False,NotApplicable),solve(node.left,False,NotApplicable)+solve(node.right,True,True),solve(node.left,True,False)+solve(node.right,True,False))
+                return 1+min(
+                    solve(node.left,True,True)+solve(node.right,False,NotApplicable),
+                    solve(node.left,False,NotApplicable)+solve(node.right,True,True),
+                    solve(node.left,True,False)+solve(node.right,True,False)
+                )
         
         return solve(root,False,NotApplicable)

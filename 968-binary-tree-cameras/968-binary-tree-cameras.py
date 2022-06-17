71df224fa5cd5704488f95e9062b0ddb556baeb2
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-17 12:53:53 +0530

    Time: 113 ms (5.07%), Space: 17 MB (5.36%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.py
+++ 968-binary-tree-cameras/968-binary-tree-cameras.py
@@ -1,17 +1,16 @@
 class Solution:
     def minCameraCover(self, root: Optional[TreeNode]) -> int:
+        NotApplicable = None
         @cache
-        def solve(node,amMonitored,throughCur):
+        def solve(node,amMonitored,monitoredThroughCur):
             if not node: return 0
             if amMonitored:
-                if throughCur:
+                if monitoredThroughCur:
                     return solve(node.left,True,False)+solve(node.right,True,False)
                 else:
-                    return min(solve(node.left,False,False)+solve(node.right,False,False)
+                    return min(solve(node.left,False,NotApplicable)+solve(node.right,False,NotApplicable)
                                , 1+solve(node.left,True,False)+solve(node.right,True,False))
             else:
-                # if not node.left and not node.right:
-                #     return 1
-                # else:
-                return 1+min(solve(node.left,True,True)+solve(node.right,False,None),solve(node.left,False,None)+solve(node.right,True,True),solve(node.left,True,False)+solve(node.right,True,False))
-        return solve(root,False,None)
+                return 1+min(solve(node.left,True,True)+solve(node.right,False,NotApplicable),solve(node.left,False,NotApplicable)+solve(node.right,True,True),solve(node.left,True,False)+solve(node.right,True,False))
+        
+        return solve(root,False,NotApplicable)

116a22d5f59fc2346557f327a9a961a469ad9013
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-18 14:39:23 +0530

    Time: 51 ms (57.21%), Space: 14 MB (12.54%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -1,20 +1,27 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# class Solution:
+#     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+#         n = len(preorder)
+#         def solve(l,r):
+#             if l<=r:
+#                 cur = preorder[l]
+#                 toPlace = bisect_left(preorder,cur,l+1,r+1)
+#                 node = TreeNode(cur)
+#                 node.left = solve(l+1,toPlace-1)
+#                 node.right = solve(toPlace,r)
+#                 return node
+#         return solve(0,n-1)
+
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         n = len(preorder)
+        idx=0
         
-        def solve(l,r):
-            if l<=r:
-                cur = preorder[l]
-                toPlace = bisect_left(preorder,cur,l+1,r+1)
-                node = TreeNode(cur)
-                node.left = solve(l+1,toPlace-1)
-                node.right = solve(toPlace,r)
-                return node
-        
-        return solve(0,n-1)
+        def helper(upperBound=inf):
+            nonlocal idx
+            if idx==n or preorder[idx]>=upperBound:return None
+            node = TreeNode(preorder[idx])
+            idx+=1
+            node.left = helper(node.val)
+            node.right = helper(upperBound)
+            return node
+        return helper() 

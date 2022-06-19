47e91b81dea0cfe4f40c78a99ca656e029532be0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-19 12:12:29 +0530

    Time: 249 ms (55.34%), Space: 20.6 MB (39.81%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,24 +1,24 @@
 from sortedcontainers import SortedSet
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        products.sort()
         TR = lambda : defaultdict(TR)
         trie = TR()
         ITEMS = "items"
-        for product in products:
+        for idx,product in enumerate(products):
             tmp = trie
             for c in product:
                 tmp = tmp[c]
                 if ITEMS not in tmp:
-                    tmp[ITEMS] = SortedSet()
-                tmp[ITEMS].add(product)
-                if len(tmp[ITEMS])>3:
-                    tmp[ITEMS].pop()
+                    tmp[ITEMS] = []
+                if len(tmp[ITEMS])<3:
+                    tmp[ITEMS].append(idx)                    
         ans = []
         tmp = trie
         for w in searchWord:
             tempans = []
             tmp = tmp[w]
             if ITEMS in tmp:
-                tempans.extend(tmp[ITEMS])
+                tempans.extend([products[i] for i in tmp[ITEMS]])
             ans.append(list(tempans))
         return ans

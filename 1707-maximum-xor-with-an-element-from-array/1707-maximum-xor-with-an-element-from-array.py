20312e31708bff68bc3fdf654ec7f983b5d0b90d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-20 15:58:32 +0530

    Time: 8222 ms (24.14%), Space: 388 MB (5.17%) - LeetHub

--- 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.py
+++ 1707-maximum-xor-with-an-element-from-array/1707-maximum-xor-with-an-element-from-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def closest(self,trie,num):
-        if len(trie.keys())==0: return -1
+        if len(trie)==0: return -1
         ans,tmp = 0,trie
-        for i in range(self.TLEN-1,-1,-1):
+        for i in range(self.MAXLEN-1,-1,-1):
             bit = (num>>i)&1
             flippedBit = 1-bit
             if flippedBit in tmp:
@@ -15,22 +15,19 @@ class Solution:
     
     def insert(self,trie,num):
         tmp = trie
-        for i in range(self.TLEN-1,-1,-1):
+        for i in range(self.MAXLEN-1,-1,-1):
             bit = (num>>i)&1
             tmp = tmp[bit]
             
     def maximizeXor(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        self.TLEN = len(bin(max(max(nums),max([q[0] for q in queries]))))-2
+        self.MAXLEN = len(bin(max(max(nums),max([q[0] for q in queries]))))-2
         Trie = lambda : defaultdict(Trie)
         nums.sort()
         trie,iter,ans = Trie(),0,[None]*len(queries)
-        base,ub,origIdx = range(3)
-        queries = sorted([(q[0],q[1],idx) for idx,q in enumerate(queries)],key=lambda x:x[ub])
+        queries = sorted([(q[0],q[1],idx) for idx,q in enumerate(queries)],key=lambda x:x[1])
         for Base,UB,OrigIdx in queries:
             while iter<len(nums) and nums[iter]<=UB:
-                self.insert(trie,nums[iter])
-                iter+=1
-            # print(iter,Base,nums)
+                _,iter = self.insert(trie,nums[iter]),iter+1
             temp = self.closest(trie,Base)
             ans[OrigIdx] = temp
         return ans

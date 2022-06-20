833868977d65e496f6993b50b5a4941a0f389105
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-20 14:46:25 +0530

    Time: 6697 ms (20.35%), Space: 103.4 MB (19.80%) - LeetHub

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findClosest(self,trie,target):
+        tmp = trie
+        ans = 0
+        for bit in target:
+            if bit in tmp:
+                ans = ans|1
+                tmp = tmp[bit]
+            else:
+                tmp = tmp[1-bit]
+            ans<<=1
+        return ans
+    def insert(self,trie,num):
+        tmp = trie
+        for bit in num:
+            tmp = tmp[bit]
+    def findMaximumXOR(self, nums: List[int]) -> int:
+#      biggest num binary representation length in array
+        n = len(bin(max(nums)))-2
+        Trie = lambda : defaultdict(Trie)
+        trie = Trie()
+#       redefine nums
+        nums = [[int((num & (1<<i))!=0) for i in range(n-1,-1,-1)] for num in nums]
+        # print(nums)
+        ans = 0
+        for num in nums:
+            flipped = [1-bit for bit in num]
+            closest = self.findClosest(trie,flipped) 
+            ans = max(ans,closest)
+            self.insert(trie,num)
+        return ans//2

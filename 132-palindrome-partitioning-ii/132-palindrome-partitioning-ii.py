16091dc6e0596e801bdd7cdc9e031f1cf355bbde
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-22 16:07:52 +0530

    Time: 2174 ms (45.17%), Space: 466.7 MB (6.78%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -3,11 +3,11 @@ class Solution:
         n = len(string)
         @cache
         def isPalin(l,r):
-            if l>=r:
-                return True
+            if l>=r:return True
             return string[l]==string[r] and isPalin(l+1,r-1)
         @cache
         def solve(i):
+            if i==n: return 0
             if isPalin(i,n-1):
                 return 0
             else:

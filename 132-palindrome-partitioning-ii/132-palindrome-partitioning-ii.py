6754e7e02bccde5a308b17cd8e06ad7c71463fe3
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-22 16:06:04 +0530

    Time: 2341 ms (42.27%), Space: 463.8 MB (7.85%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.py
@@ -1,23 +1,22 @@
 class Solution:
     def minCut(self, string: str) -> int:
         n = len(string)
-        # dp = [[-1]*n for i in range(n)]
         @cache
         def isPalin(l,r):
             if l>=r:
                 return True
             return string[l]==string[r] and isPalin(l+1,r-1)
         @cache
-        def solve(l,r):
-            # if dp[l][r]!=-1:return dp[l][r]
-            if l>r or isPalin(l,r):
-                # dp[l][r] = 0
+        def solve(i):
+            if i==n:
+                return -1
+            if isPalin(i,n-1):
                 return 0
             else:
                 ans = inf
-                for midEnd in range(l,r):
-                    if isPalin(l,midEnd):
-                        ans = min(ans,solve(midEnd+1,r))
-                # dp[l][r] = 1+ans
-                return 1+ans
-        return solve(0,len(string)-1)
+                for midEnd in range(i,n-1):
+                    if isPalin(i,midEnd):
+                        ans = min(ans,1+solve(midEnd+1))
+                return ans
+                    
+        return solve(0)

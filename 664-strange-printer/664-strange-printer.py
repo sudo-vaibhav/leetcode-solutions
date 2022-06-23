526b9980cc90ed7f9f1328d0e374ace86066421c
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-23 14:27:02 +0530

    Time: 664 ms (63.02%), Space: 16.9 MB (47.21%) - LeetHub

--- 664-strange-printer/664-strange-printer.py
+++ 664-strange-printer/664-strange-printer.py
@@ -4,7 +4,7 @@ class Solution:
         def solve(start,end):
             if end<start: return 0
             if end==start: return 1
-            if end-start==1: return 1 if s[start]==s[end] else 2
+            # if end-start==1: return 1 if s[start]==s[end] else 2
             ans = 1+solve(start+1,end)
             for k in range(start+1,end+1):
                 if s[k]==s[start]:

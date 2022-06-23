9b197eca2ebb287bdc5bce27d611167eea5c2abb
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-23 18:00:47 +0530

    Time: 212 ms (67.33%), Space: 14.4 MB (74.48%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,18 +1,31 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        color = {}
-        def dfs(node,col):
-            color[node]=col
-            for dest in graph[node]:
-                if dest in color:
-                    if color[dest]==col:
-                        return False
-                else:
-                    if not dfs(dest,1-col):return False
-            return True
+#         color = {}
+#         def dfs(node,col):
+#             color[node]=col
+#             for dest in graph[node]:
+#                 if dest in color:
+#                     if color[dest]==col: return False
+#                 else:
+#                     if not dfs(dest,1-col):return False
+#             return True
         
-        for i in range(len(graph)):
-            if i not in color:
-                if not dfs(i,0):
-                    return False
+#         for node in range(len(graph)):
+#             if node not in color:
+#                 if not dfs(node,0): return False
+#         return True
+        color = {}
+        q = deque()
+        for node in range(len(graph)):
+            if node not in color:
+                q.append(node)
+                color[node] = 0
+                while q:
+                    curNode = q.popleft()
+                    for dest in graph[curNode]:
+                        if dest in color:
+                            if color[dest]==color[curNode]:return False
+                        else:
+                            color[dest]=1-color[curNode]
+                            q.append(dest)
         return True

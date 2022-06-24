59dbb2b303a77df1426f19f0954c0b028fef8efd
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-24 14:18:33 +0530

    Time: 395 ms (56.47%), Space: 20 MB (25.88%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.py
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.py
@@ -6,16 +6,9 @@ class Solution:
         while heap[0]!=-1:
             cur = -heappop(heap)
             adjustedSum = S-cur
-            # prev = S-2*cur
-            # to get prev value we need to do cur - (S-cur)
-            # and push the new value back in heap, but if cur itself is too big
-            # the same element will come again to be subtracted, so we use module to
-            # cascade operations and make function faster
-            # but module doesnt give correct result when divisor is 1 or lesser (1 always 
-            # can reduce anything to any desired value)
-            # if S-cur<=1:
-            #     return bool(S-cur)
             if cur<= adjustedSum or adjustedSum<1:return False
+            if adjustedSum==1:
+                return True
             adjustedCur = cur%adjustedSum
 #           now readjust S value
             S = adjustedSum + adjustedCur

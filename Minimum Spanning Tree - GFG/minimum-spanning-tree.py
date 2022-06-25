35e445c4e651d2e8df7817a1253d2a62c21802f3
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-25 11:37:46 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.py
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.py
@@ -1,40 +1,54 @@
-#User function Template for python3
-
+from heapq import heappush,heappop
+from math import inf
 class Solution:
+    # def spanningTree(self, V, adj):
+    #     class UF:
+    #         def __init__(self,V):
+    #             self.parent = [i for i in range(V)]
+    #             self.rank = [0 for _ in range(V)]
+    #         def union(self,u,v):
+    #             pu,pv = self.find(u),self.find(v)
+    #             if pu==pv:return
+    #             ru,rv = self.rank[pu],self.rank[pv]
+    #             if ru<rv:
+    #                 pu,pv = pv,pu
+    #                 ru,rv = rv,ru
+    #             self.parent[pv] = pu
+    #             if ru==rv:
+    #                 self.rank[pu]+=1
+    #         def find(self,u):
+    #             if self.parent[u]==u:return u
+    #             self.parent[u] = self.find(self.parent[u])
+    #             return self.parent[u]
+    #     uf = UF(V)
+    #     edges = []
+    #     ans = 0
+    #     for i in range(V):
+    #         for dest,wt in adj[i]:
+    #             edges.append((i,dest,wt))
+    #     edges.sort(key=lambda x :x[2])
+    #     for u,v,wt in edges:
+    #         if uf.find(u)==uf.find(v): continue
+    #         ans += wt
+    #         uf.union(u,v)
+    #     return ans
     
-    #Function to find sum of weights of edges of the Minimum Spanning Tree.
     def spanningTree(self, V, adj):
-        class UF:
-            def __init__(self,V):
-                self.parent = [i for i in range(V)]
-                self.rank = [0 for _ in range(V)]
-            def union(self,u,v):
-                pu,pv = self.find(u),self.find(v)
-                if pu==pv:return
-                ru,rv = self.rank[pu],self.rank[pv]
-                if ru<rv:
-                    pu,pv = pv,pu
-                    ru,rv = rv,ru
-                self.parent[pv] = pu
-                if ru==rv:
-                    self.rank[pu]+=1
-            def find(self,u):
-                if self.parent[u]==u:return u
-                self.parent[u] = self.find(self.parent[u])
-                return self.parent[u]
-        uf = UF(V)
-        edges = []
-        for i in range(V):
-            for dest,wt in adj[i]:
-                edges.append((i,dest,wt))
-        edges.sort(key=lambda x :x[2])
-        ans = 0
-        for u,v,wt in edges:
-            if uf.find(u)==uf.find(v): continue
-            ans += wt
-            uf.union(u,v)
-        return ans
-                    
+        heap = []
+        mstSet = [False]*V
+        dist = [inf]*V
+        parent = [-1]*V
+        dist[0]=0
+        heappush(heap,(0,0))
+        while heap:
+            _,u = heappop(heap)
+            mstSet[u]=True
+            for v,wt in adj[u]:
+                if mstSet[v]==False and dist[v]>wt:
+                    dist[v] = wt
+                    heappush(heap,(dist[v],v))
+        
+        return sum(dist)
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3

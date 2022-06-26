734c51744202c78dd176859e09e711012171ebb7
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-26 15:41:25 +0530

    Time: 917 ms (10.99%), Space: 67.9 MB (34.58%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -1,25 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
         ans = 0
-#         isBST, minVal, maxVal, sum
+        isBST, minVal, maxVal, sum = range(4)
         def solve(node):
             nonlocal ans
-            if not node:
-                return (True,inf,-inf,0)
-            # if not node.left and not node.right: 
-            #     return (True,node.val,node.val,node.val)
-            L = solve(node.left)
-            R = solve(node.right)
-            if L[0] and R[0] and L[2]<node.val<R[1]:
-                tempSum = L[3]+R[3]+node.val
+            if not node: return (True,inf,-inf,0)
+            L,R = solve(node.left),solve(node.right)
+            if L[isBST] and R[isBST] and L[maxVal]<node.val<R[minVal]:
+                tempSum = L[sum]+R[sum]+node.val
                 ans = max(ans,tempSum)
-                return (True,min(node.val,L[1],R[1]),max(node.val,L[2],R[2]),tempSum)
+                return (True,min(node.val,L[minVal],R[minVal]),max(node.val,L[maxVal],R[maxVal]),tempSum)
             return tuple([False])
         
         solve(root)

5fff3de0f940e2b2b8c358c18ec8da9c80d18d14
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-26 15:59:41 +0530

    Time: 161 ms (23.67%), Space: 16.5 MB (71.97%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -0,0 +1,33 @@
+class BSTIterator:
+    def __init__(self, root: Optional[TreeNode],traversal):
+        self.st = []
+        self.trav = traversal
+        temp = root
+        while temp:
+            self.st.append(temp)
+            temp = temp.left if self.trav=="lvr" else temp.right
+    def peek(self):
+        return self.st[-1].val
+    def next(self) -> int:
+        node = self.st.pop()
+        temp = node.right if self.trav=="lvr" else node.left
+        while temp:
+            self.st.append(temp)
+            temp = temp.left if self.trav=="lvr" else temp.right
+        return node.val
+    # def hasNext(self) -> bool:
+    #     return self.st
+
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        l,r = BSTIterator(root,"lvr"),BSTIterator(root,"rvl")      
+        while True:
+            L,R = l.peek(),r.peek()
+            if L>=R:
+                break
+            temp = L+R
+            if temp==k: return True
+            elif temp<k:l.next()
+            else: r.next()
+                
+        return False

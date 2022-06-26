f5d1664348f34ea774eb0ffce647fb2a130fb614
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-06-26 15:49:08 +0530

    Time: 722 ms (29.45%), Space: 68.3 MB (24.77%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -9,7 +9,7 @@ class Solution:
             if L[isBST] and R[isBST] and L[maxVal]<node.val<R[minVal]:
                 tempSum = L[sum]+R[sum]+node.val
                 ans = max(ans,tempSum)
-                return (True,min(node.val,L[minVal],R[minVal]),max(node.val,L[maxVal],R[maxVal]),tempSum)
+                return (True,min(node.val,L[minVal]),max(node.val,R[maxVal]),tempSum)
             return tuple([False,"""rest params dont matter anyways now"""])
         solve(root)
         return ans

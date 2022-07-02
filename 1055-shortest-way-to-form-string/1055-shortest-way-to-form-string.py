405072ed73773fcf14b371c5af844503f2fd3df5
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-02 16:10:31 +0530

    Time: 121 ms (19.08%), Space: 18.6 MB (5.48%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -3,23 +3,37 @@ class Solution:
         sc,tc = set(source),set(target) # O(1) space
         for c in tc: # O(1)
             if c not in sc: return -1
+        m = len(source)
         n = len(target)
         occs = defaultdict(list) # O(1)
-        for idx,i in enumerate(source): # O(n)
-            # if i not in occ:
-            occs[i].append(idx)
-        m = len(source)
+        nex = defaultdict(lambda : m)
+        
+        latestOcc = {}
+        for idx in range(m-1,-1,-1):
+            cur = source[idx]
+            latestOcc[cur] = idx
+            for alpha in range(ord('a'),ord('z')+1):
+                alp = chr(alpha)
+                if alp in latestOcc:
+                    # print(idx,alp)
+                    nex[idx,alp] = latestOcc[alp]
+                    
         ti = 0
         si = 0
         ans = 1
+        
+#       with candidates starting from current index
+        def getNextOcc(char,idx):
+            # print(char,idx)
+            return nex[idx,char]
+        
         while ti<n: # O(n)
             curChar = target[ti]
-            newSi = bisect_left(occs[curChar],si)
-            # print(curChar,si,newSi)
-            if newSi >= len(occs[curChar]):
+            newSi = getNextOcc(curChar,si)
+            if newSi >= m:
                 ans+=1
                 si = 0
                 continue
-            si = occs[curChar][newSi]+1
+            si = newSi+1
             ti+=1
         return ans

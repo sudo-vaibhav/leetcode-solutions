ffe3b2ff2158df965b5193340af2981f6280881a
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-08 17:46:27 +0530

    Time: 1553 ms (65.12%), Space: 44.6 MB (26.36%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.py
+++ 1473-paint-house-iii/1473-paint-house-iii.py
@@ -1,24 +1,20 @@
 class Solution:
     def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
         houseCnt = len(houses)
-        # cost[house][color+1]
         @cache
-        def solve(i,prev,grpSoFar):
-            if i==houseCnt:
+        def solve(idx,prev,grpSoFar):
+            if grpSoFar>target or idx==houseCnt:
                 if grpSoFar==target:
                     return 0
                 else:
                     return inf
-            if grpSoFar>target:
-                return inf
-            curCol = houses[i]
-            
+            curCol = houses[idx]
             if curCol!=0:
-                return solve(i+1,curCol,grpSoFar + (1 if curCol!=prev else 0))
+                return solve(idx+1,curCol,grpSoFar + (1 if curCol!=prev else 0))
             else:
                 ans = inf
                 for curCol in range(1,n+1):
-                    ans = min(ans, cost[i][curCol-1] + solve(i+1,curCol,grpSoFar + (1 if curCol!=prev else 0)))
+                    ans = min(ans, cost[idx][curCol-1] + solve(idx+1,curCol,grpSoFar + (1 if curCol!=prev else 0)))
                 return ans
         
         res = solve(0,inf,0)

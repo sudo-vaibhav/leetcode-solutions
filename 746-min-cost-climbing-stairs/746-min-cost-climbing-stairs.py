1ee2c514f955ded442d9046c172aaab38e37505e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-10 13:41:17 +0530

    Time: 113 ms (21.35%), Space: 14 MB (44.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,14 +1,25 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         n = len(cost)
+        
+#         @cache
+#         def solve(i):
+#             if i>=n:return 0
+#             return cost[i] + min(
+#                 solve(i+2),
+#                 solve(i+1) #if i+1<n else inf
+#             )
+        
+#         return min(solve(0),solve(1))
+
         n = len(cost)
+        endCost = [0]*n
+        endCost[-1] = 0
+        endCost[-2] = 0
         
-        @cache
-        def solve(i):
-            if i>=n:return 0
-            
-            return min(
-                cost[i]+solve(i+2),
-                (cost[i]+solve(i+1)) #if i+1<n else inf
-            )
+        for i in range(n-3,-1,-1):
+            endCost[i] = min(endCost[i+1]+cost[i+1],endCost[i+2]+cost[i+2])
         
-        return min(solve(0),solve(1))
+        # print(endCost)
+        return min(endCost[0]+cost[0],endCost[1]+cost[1])
+        

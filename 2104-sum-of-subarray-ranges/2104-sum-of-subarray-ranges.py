bfd9503da8b8d4f008b55b2c4033bfe901206b7d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-12 18:58:52 +0530

    Time: 260 ms (52.25%), Space: 14.2 MB (47.26%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -1,12 +1,64 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
         
-        ans = 0
+#         ans = 0
+#         n = len(nums)
+#         for start in range(n):
+#             mini,maxi = nums[start],nums[start]
+#             for end in range(start,n):
+#                 maxi = max(maxi,nums[end])
+#                 mini = min(mini,nums[end])
+#                 ans += maxi-mini
+#         return ans
+
+
+        st = []
         n = len(nums)
-        for start in range(n):
-            mini,maxi = nums[start],nums[start]
-            for end in range(start,n):
-                maxi = max(maxi,nums[end])
-                mini = min(mini,nums[end])
-                ans += maxi-mini
+        nextGreater = [n]*n
+        nextSmaller = [n]*n
+        prevSmaller = [-1]*n
+        prevGreater = [-1]*n
+        
+        for idx in range(n-1,-1,-1):
+            while st and nums[st[-1]]>=nums[idx]:
+                st.pop()
+            if st:
+                nextSmaller[idx] = st[-1]
+            st.append(idx)
+        
+        st = []
+        
+        
+        for idx in range(n-1,-1,-1):
+            while st and nums[st[-1]]<=nums[idx]:
+                st.pop()
+            if st:
+                nextGreater[idx] = st[-1]
+            st.append(idx)
+        st = []
+        for idx in range(n):
+            while st and nums[st[-1]]<nums[idx]:
+                st.pop()
+            if st:
+                prevGreater[idx] = st[-1]
+            st.append(idx)
+        
+        st = []
+        for idx in range(n):
+            while st and nums[st[-1]]>nums[idx]:
+                st.pop()
+            if st:
+                prevSmaller[idx] = st[-1]
+            st.append(idx)
+        
+        ans = 0
+        # print(nextGreater,prevGreater,nextSmaller,prevSmaller)
+        for i in range(n):
+            cur = nums[i]
+            plus,minus = (i-prevGreater[i])*(nextGreater[i]-i),(i-prevSmaller[i])*(nextSmaller[i]-i)
+            ans += plus*cur
+            ans -= minus*cur
+            # print(plus,minus,i)
+        
         return ans
+        

c35bd1df8905379e97ae9217cebf15f0e3b9d8de
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-13 13:47:17 +0530

    Time: 3213 ms (5.04%), Space: 362 MB (20.15%) - LeetHub

--- 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -8,16 +8,10 @@ class Solution:
         def solve(fightIdx,canUseArmor):
             if fightIdx<0:
                 return 0
-            if fightIdx == n-1:
-                ex = 1
-            else:
-                ex = 0
             dam = damage[fightIdx]
             ans = inf
             if canUseArmor:
-                ans = min(ans,ex+max(0,dam-armor)+solve(fightIdx-1,False))
-            ans = min(ans,ex+dam+solve(fightIdx-1,canUseArmor))
-            
+                ans = min(ans,max(0,dam-armor)+solve(fightIdx-1,False))
+            ans = min(ans,dam+solve(fightIdx-1,canUseArmor))
             return ans
-        
-        return solve(n-1,True)
+        return 1+solve(n-1,True)

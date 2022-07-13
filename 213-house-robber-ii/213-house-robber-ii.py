7123848a7e451f6bb2852fad657cc58b0df97c60
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-13 22:09:33 +0530

    Time: 56 ms (33.47%), Space: 13.9 MB (24.40%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -5,20 +5,14 @@ class Solution:
             return nums[0]
         
         
-        def solve(nums):
+        def solve(L,R):
             secondLast,last = 0,0
             cur = 0
-            for i in range(len(nums)):
+            for i in range(L,R+1):
                 cur = max(nums[i]+secondLast,last)
                 secondLast = last
                 last = cur
-            
             return cur
-            # if i<0:
-            #     dp[i] = 0
-            #     return  0
-            # else:
-            #     dp[i] = max(nums[i]+solve(nums,i-2,dp),solve(nums,i-1,dp))
-            #     return dp[i]
         
-        return max(solve(nums[1:]),solve(nums[:-1]))
+        n = len(nums)
+        return max(solve(1,n-1),solve(0,n-2))

70bfb94df429b9cec3ee94aae0fa18a30993efc5
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-14 14:45:05 +0530

    Time: 44 ms (89.15%), Space: 14 MB (60.57%) - LeetHub

---
+++ 772-basic-calculator-iii/772-basic-calculator-iii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        
+        i,num,sign,st = 0,0,"+",[]
+        def update(num,sign):
+            if sign=="+":
+                st.append(num)
+            elif sign == "-":
+                st.append(-num)
+            elif sign == "*":
+                st.append(st.pop()*num)
+            elif sign == "/":
+                st.append(int(st.pop()/num))
+        
+        while i<len(s):
+            cur = s[i]
+            if cur.isdigit():
+                num = num*10+int(cur)
+            elif cur in "+/-*":
+                update(num,sign)
+                num,sign = 0,cur
+            elif cur == "(":
+                num,j = self.calculate(s[i+1:])
+                i = i+j
+            elif cur == ")":
+                update(num,sign)
+                return sum(st),i+1
+            i+=1
+        update(num,sign)
+        return sum(st)
+            

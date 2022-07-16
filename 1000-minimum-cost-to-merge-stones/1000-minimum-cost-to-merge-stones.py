9b0a2cf8428f1b29b8b090da6a3d5c34d01f7be9
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-16 22:52:28 +0530

    Time: 336 ms (12.96%), Space: 16.4 MB (8.31%) - LeetHub

--- 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
+++ 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
@@ -4,7 +4,11 @@ class Solution:
         N = len(stones)
         
 #         N - v(k-1) = 1
-
+        pref = [0]
+        for num in stones:
+            pref.append(pref[-1]+num)
+        def getSum(i,j):
+            return pref[j+1]-pref[i]
         if (N-1)%(k-1)==0:
             @cache
             def solve(i,j,piles):
@@ -18,12 +22,12 @@ class Solution:
                     if piles==1:
                         temp = solve(i,j,k)
                         if temp!=inf:
-                            ans = temp + sum(stones[i:j+1])
+                            ans = temp + getSum(i,j)
                     else:
                         for end in range(i,j):
                             l = solve(i,end,piles-1)
                             r = solve(end+1,j,1)
-                            ans = min(ans,l+r)#+sum(stones[i:j+1]))
+                            ans = min(ans,l+r)
                 return ans
                         
             return solve(0,N-1,1)

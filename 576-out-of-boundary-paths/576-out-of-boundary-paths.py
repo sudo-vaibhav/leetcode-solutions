d2673c5257364cc596821bd3594db8db24b347de
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-16 09:02:55 +0530

    Time: 1178 ms (5.11%), Space: 34.8 MB (5.37%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        dp = defaultdict(int)
+        
+        for col in range(n):
+            if col==0 or col==n-1:
+#                 full mark case
+                for i in range(m):
+                    dp[i,col,1] = 1
+            else:
+#                 non full mark case
+                dp[0,col,1] = 1
+                dp[m-1,col,1] = 1
+        
+        dp[0,0,1] = 2
+        dp[m-1,n-1,1] = 2
+        dp[0,n-1,1]=2
+        dp[m-1,0,1]=2
+        MOD = 10**9+7
+        
+        if m==1 or n==1:
+            if m==1 and n==1:
+                dp[0,0,1] = 4
+            else:
+                for i in range(m):
+                    for j in range(n):
+                        dp[i,j,1]+=1
+        moves = [(-1,0),(1,0),(0,1),(0,-1)]
+        for movesCount in range(2,maxMove+1):
+            for i in range(m):
+                for j in range(n):
+                    for di,dj in moves:
+                        I,J = i+di,j+dj
+                        if 0<=I<m and 0<=J<n:
+                            dp[i,j,movesCount] = (dp[I,J,movesCount-1]+dp[i,j,movesCount])%MOD 
+        ans= 0
+        for i in range(1,maxMove+1):
+            ans = (ans+dp[startRow,startColumn,i])%MOD
+        
+        return ans
+            

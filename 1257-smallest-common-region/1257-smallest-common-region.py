b1783189e4c4dba6c4aaebdc8d6b8d530af14fb0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-19 11:32:21 +0530

    Time: 291 ms (71.88%), Space: 19 MB (74.57%) - LeetHub

--- 1257-smallest-common-region/1257-smallest-common-region.py
+++ 1257-smallest-common-region/1257-smallest-common-region.py
@@ -2,11 +2,6 @@ class Node:
     def __init__(self,place):
         self.place = place
         self.parent = None
-        # self.children = []
-    # def addChild(self,child):
-    #     self.children.append
-    # def __str__(self):
-    #     return ("("+self.place+"=>"+(self.parent if self.parent!=None else "no parent")+")")
 class Solution:
     def findSmallestRegion(self, regions: List[List[str]], r1: str, r2: str) -> str:
         nodes = {}
@@ -21,13 +16,10 @@ class Solution:
                 if child not in nodes:
                     nodes[child] = Node(child)
                     nodes[child].parent = nodes[parent]
-                # nodes[parent].addChild(nodes[child])
         seen = set()
         def lca(r1,r2):
-            # print("printing r1,r2",r1,r2)
             temp = r1
             while temp:
-                # print(temp,type(temp))
                 seen.add(temp.place)
                 temp = temp.parent
             temp = r2
@@ -36,10 +28,5 @@ class Solution:
                     return temp.place
                 temp = temp.parent
                 
-        # print(nodes)
-        # for k in nodes:
-        #     print(k,str(nodes[k]))
-        # return ""
         return lca(nodes[r1],nodes[r2])
-        # return 
         

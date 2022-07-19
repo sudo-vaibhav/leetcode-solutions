c1542ce60cac14ac1cd7b430c2f8b3e36872af50
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-19 16:40:12 +0530

    Time: 1893 ms (34.75%), Space: 19.2 MB (94.07%) - LeetHub

---
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        m,n = len(mat),len(mat[0])
+        ans = 0
+        lc,rc= 0,0
+        
+        psum = deepcopy(mat)
+        for i in range(m):
+            for j in range(1,n):
+                psum[i][j] += psum[i][j-1]
+        ans = 0
+        success = defaultdict(int)
+        
+        while rc<n:
+            wsize = rc-lc+1
+            def isPos(lc,rc,wsize):
+                rowSums = []
+                for row in range(m):
+                    rowSums.append(psum[row][rc]-(psum[row][lc-1] if lc>=1 else 0))
+                running = 0
+                for x in range(m):
+                    running += rowSums[x]
+                    if x>=wsize-1:
+                        if running<=threshold:
+                            return True
+                        running -= rowSums[x-wsize+1]
+                return False
+            if wsize<=m and isPos(lc,rc,wsize):
+                ans = max(ans,wsize)
+            else:
+                lc+=1
+            # print(success)
+            rc+=1
+        
+        
+#         for size in range(1,min(m,n)):
+#             windowsCount = n-size+1
+#             if success[size]==windowsCount:
+#                 ans = max(ans,size)
+        
+        return ans

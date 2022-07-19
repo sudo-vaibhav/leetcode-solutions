8fa43c880d8f7db2cbdfb90602b9b6e50c9d2ece
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-19 10:57:02 +0530

    Time: 1115 ms (18.99%), Space: 13.9 MB (85.23%) - LeetHub

---
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSubstrings(self, s: str, t: str) -> int:
+        m,n = len(s),len(t)
+        oneOff,allEql = 0,1
+        def solve(i,j):
+            if i<0 or j<0:
+                return (0,0)
+            prev = solve(i-1,j-1)
+            res = [None,None]
+            if s[i]==t[j]:
+                res[oneOff] = prev[oneOff]
+                res[allEql] = prev[allEql]+1
+            else:
+                res[oneOff] = prev[allEql]+1
+                res[allEql] = 0
+            return res
+                
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                ans += solve(i,j)[0]
+        return ans                         

8d988e1d725896ac15ab9e27cb6128716784f98e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-24 12:05:19 +0530

    Time: 2213 ms (25.00%), Space: 29.5 MB (100.00%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.py
@@ -8,20 +8,11 @@ class Solution:
                 num &= num-1
                 t+=1
             d[t].add(orig)
-        
-        # seen = set()
         ans = 0
         for first in d:
-            # rest = k-first
-            for num in d[first]:
-                # d[first].remove(num)
-                for second in d:
-                    if first+second>=k:
-                        ans += len(d[second])
-                
-                # d[first].add(num)
-        
+            # for num in d[first]:
+            for second in d:
+                if first+second>=k:
+                    ans += len(d[second])*len(d[first])
         return ans
-            # d[first]
-            # print(num,t)
         

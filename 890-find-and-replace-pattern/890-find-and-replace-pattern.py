e5d17d68984d444896bdbd308f6a236141bc3137
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-29 10:40:35 +0530

    Time: 63 ms (22.49%), Space: 14 MB (28.72%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -1,24 +1,50 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        base = 26
+        MOD = 2**63-1
+#         def getAlloc(s):
+#             n = len(s)
+#             ans = []
+#             alloc = {}
+#             c = 0
+#             for i in s:
+#                 if i in alloc:
+#                     pass
+#                 else:
+#                     alloc[i]=c
+#                     c+=1
+#                 ans.append(alloc[i])
+#             return ans
         
+#         main = getAlloc(pattern)
+#         res = []
+#         for w in words:
+#             if getAlloc(w)==main:
+#                 res.append(w)
+        
+#         return res
         def getAlloc(s):
             n = len(s)
-            ans = []
+            ans = 0
             alloc = {}
             c = 0
             for i in s:
+                ans = (ans*base)%MOD
                 if i in alloc:
                     pass
                 else:
                     alloc[i]=c
                     c+=1
-                ans.append(alloc[i])
+
+                ans = (ans+alloc[i])%MOD
             return ans
-        
+
         main = getAlloc(pattern)
+#         for w in words:
+            
         res = []
         for w in words:
             if getAlloc(w)==main:
                 res.append(w)
-        
+
         return res

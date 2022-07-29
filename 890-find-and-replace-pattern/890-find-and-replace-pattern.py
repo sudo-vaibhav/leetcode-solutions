cf93c047ff9527aaca457b39f8cc610b5a678b8e
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-29 10:43:53 +0530

    Time: 40 ms (79.93%), Space: 14 MB (28.72%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -2,49 +2,16 @@ class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
         base = 26
         MOD = 2**63-1
-#         def getAlloc(s):
-#             n = len(s)
-#             ans = []
-#             alloc = {}
-#             c = 0
-#             for i in s:
-#                 if i in alloc:
-#                     pass
-#                 else:
-#                     alloc[i]=c
-#                     c+=1
-#                 ans.append(alloc[i])
-#             return ans
-        
-#         main = getAlloc(pattern)
-#         res = []
-#         for w in words:
-#             if getAlloc(w)==main:
-#                 res.append(w)
-        
-#         return res
         def getAlloc(s):
-            n = len(s)
-            ans = 0
-            alloc = {}
-            c = 0
+            ans,c,alloc = 0,0,{}
             for i in s:
                 ans = (ans*base)%MOD
-                if i in alloc:
-                    pass
-                else:
+                if i not in alloc:
                     alloc[i]=c
                     c+=1
-
-                ans = (ans+alloc[i])%MOD
+                ans += alloc[i]
+                if ans>=MOD:
+                    ans -= MOD
             return ans
-
         main = getAlloc(pattern)
-#         for w in words:
-            
-        res = []
-        for w in words:
-            if getAlloc(w)==main:
-                res.append(w)
-
-        return res
+        return [w for w in words if getAlloc(w)==main]

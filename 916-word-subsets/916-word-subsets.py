a9bbed63c269b4b691311a341ba4ed54d3aeb3c0
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-07-30 09:35:13 +0530

    Time: 2307 ms (5.67%), Space: 18.8 MB (23.33%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        mc=[0]*26
+        for w in words2:
+            temp = Counter(w)
+            for k in temp:
+                c=ord(k)-ord("a")
+                mc[c] = max(mc[c],temp[k])
+        ans=[]
+        #print(mc)
+        for w in words1:
+            def solve(w):
+                temp=Counter(w)
+                for idx,i in enumerate(mc):
+                    k = chr(idx+ord("a")) 
+                    if k not in temp:
+                        temp[k]=0
+                    if temp[k]<i:
+                        return False
+                return True
+            if solve(w):
+                ans.append(w)
+        
+        return ans

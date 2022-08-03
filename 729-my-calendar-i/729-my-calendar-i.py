628bb58280ff33e03e07fdd1d08b4221f9354f6d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-08-03 11:50:33 +0530

    Time: 468 ms (49.44%), Space: 15.2 MB (10.23%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -1,20 +1,19 @@
-from sortedcontainers import SortedSet
+from sortedcontainers import SortedList
 class MyCalendar:
-
     def __init__(self):
-        self.ss = [] #SortedSet()        
-    def book(self, start: int, end: int) -> bool:
-        end-=1
-        for s,e in self.ss:
-            if (start<=s<=e<=end) or (s<=start<=end<=e): # full overlap
-                return False
-            if s<=start<=e or s<=end<=e: # partial overlap
-                return False
-        self.ss.append((start,end)) # no overlap, so add
-        return True
+        self.ss = SortedList()
         
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+    def overlap(self,i1,i2):
+        begin,end = max(i1[0],i2[0]),min(i1[1],i2[1])
+        return begin<end
+    def book(self, start: int, end: int) -> bool:
+        intvl = (start,end)
+        pos = self.ss.bisect_left(intvl)
+    
+        if pos>0 and self.overlap(self.ss[pos-1],intvl):
+            return False
+        if pos<len(self.ss) and self.overlap(self.ss[pos],intvl):
+            return False
+    
+        self.ss.add(intvl)
+        return True

f867eb4834ee79cb5ef388da58aa3b5a750ebe62
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-08-09 08:44:56 +0530

    Time: 695 ms (49.49%), Space: 15.1 MB (22.22%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.py
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.py
@@ -1,18 +1,11 @@
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
-        arr = set(arr)
-        MOD = 10**9+7
+        arr,MOD = set(arr),10**9+7
         @cache
         def solve(num):
             ans = 1
-            
             for n in arr:
-                if num%n==0:
-                    rest = num//n
-                    if rest in arr:
-                        ans += solve(n)*solve(rest)
-                        if ans >= MOD:
-                            ans-=MOD
+                if num%n==0 and num//n in arr:
+                    ans = (ans+solve(n)*solve(num//n))%MOD
             return ans
-        
         return sum([solve(n) for n in arr])%MOD

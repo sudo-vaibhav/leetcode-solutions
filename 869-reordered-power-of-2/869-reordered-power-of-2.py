f70409384ae6c1c3134409149b5a0a12ef223288
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-08-26 14:08:56 +0530

    Time: 59 ms (56.15%), Space: 13.9 MB (64.17%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -2,14 +2,10 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         nums = []
         x = 1
-        
-        while x<=10**9:
-            nums.append(Counter(str(x)))
-            x*=2
-        
-        # print(nums)
         temp = Counter(str(n))
-        for n in nums:
-            if n==temp:
+        while x<=10**9:
+            cur = Counter(str(x))
+            if temp==cur:
                 return True
+            x*=2
         return False

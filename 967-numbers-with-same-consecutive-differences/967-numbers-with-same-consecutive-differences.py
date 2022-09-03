d886a1f7ff5db7e32300c63dcd229e0a77ebd55c
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-09-03 22:38:04 +0530

    Time: 56 ms (72.11%), Space: 14.1 MB (73.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,22 +1,19 @@
 class Solution:
-    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        
+    def numsSameConsecDiff(self, N: int, k: int) -> List[int]:
         ans = []
         
-        N = n
-        def solve(n,prev):
+        def solve(n,prev,path):
             if n==0:
-                ans.append(int(''.join(map(str,prev))))
-                return 
+                ans.append("".join(map(str,path)))
             else:
-                for cur in range(0,10):
-                    if cur==0 and n==N:
+                for num in range(0,10):
+                    if num==0 and n==N:
                         continue
-                    if (n==N) or (n!=N and abs(cur-prev[-1])==k):
-#                         can pick cur
-                        prev.append(cur)
-                        solve(n-1,prev)
-                        prev.pop()
+                    if n==N or abs(prev-num)==k:
+                        path.append(num)
+                        solve(n-1,num,path)
+                        path.pop()
         
-        solve(n,[])
+        solve(N,0,[])
+        
         return ans

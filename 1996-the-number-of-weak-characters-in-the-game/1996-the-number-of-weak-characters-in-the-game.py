27059b9ec0d31e1ff2656eee6bccb28431fd80f9
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-09-09 10:59:16 +0530

    Time: 7725 ms (5.04%), Space: 75.5 MB (5.79%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -1,16 +1,30 @@
 class Solution:
     def numberOfWeakCharacters(self, props: List[List[int]]) -> int:
-        n = len(props)
-        props.sort()
-        attack,defense = range(2)
-        l,r = n-1,n-1
-        ans,maxDefSeen = 0,-inf
-        while l>=0:
-            L = l
-            while l>=0 and props[l][attack]==props[L][attack]:
-                while r>=0 and props[r][attack]>props[L][attack]:
-                    maxDefSeen = max(maxDefSeen,props[r][defense])
-                    r-=1
-                if props[l][defense]<maxDefSeen:ans+=1
-                l-=1
-        return ans
+        # n = len(props)
+        # props.sort()
+        # attack,defense = range(2)
+        # l,r = n-1,n-1
+        # ans,maxDefSeen = 0,-inf
+        # while l>=0:
+        #     L = l
+        #     while l>=0 and props[l][attack]==props[L][attack]:
+        #         while r>=0 and props[r][attack]>props[L][attack]:
+        #             maxDefSeen = max(maxDefSeen,props[r][defense])
+        #             r-=1
+        #         if props[l][defense]<maxDefSeen:ans+=1
+        #         l-=1
+        # return ans
+        
+        maxDefense = defaultdict(int)
+        
+        for prop in props:
+            maxDefense[prop[0]] = max(maxDefense[prop[0]],prop[1])
+        
+        for attack in range(100_000,0,-1):
+            maxDefense[attack] = max(maxDefense[attack],maxDefense[attack+1])
+        # print(maxDefense)  
+        weak = 0
+        for prop in props:
+            if maxDefense[prop[0]+1]>prop[1]:
+                weak+=1
+        return weak

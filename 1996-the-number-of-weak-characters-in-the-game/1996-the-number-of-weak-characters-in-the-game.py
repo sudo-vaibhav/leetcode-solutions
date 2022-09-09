c3a4fec429de45c77866a1590ad42617c26c5e9a
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-09-09 10:43:02 +0530

    Time: 5504 ms (5.04%), Space: 67.1 MB (79.12%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -2,21 +2,19 @@ class Solution:
     def numberOfWeakCharacters(self, props: List[List[int]]) -> int:
         props.sort()
         n = len(props)
-        # print(props)
+        attack,defense = range(2)
         l,r = n-1,n-1
         weak = 0
-        while l>=0 and props[l][0]==props[r][0]:
-            l-=1
+        # while l>=0 and props[l][attack]==props[r][attack]:
+        #     l-=1
         maxDefSeen = -inf
         while l>=0:
             L = l
-            while l>=0 and props[l][0]==props[L][0]:
-                while r>=0 and props[r][0]>props[L][0]:
-                    maxDefSeen = max(maxDefSeen,props[r][1])
+            while l>=0 and props[l][attack]==props[L][attack]:
+                while r>=0 and props[r][attack]>props[L][attack]:
+                    maxDefSeen = max(maxDefSeen,props[r][defense])
                     r-=1
                 if props[l][1]<maxDefSeen:
                     weak+=1
                 l-=1
-                
-        # print(l,r)
         return weak

f0fa8c75ed0f4c6bcbe093e79d3960fb398e641d
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-09-10 19:21:35 +0530

    Time: 115 ms (87.59%), Space: 14 MB (79.28%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,21 +1,31 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        N = len(prices)
-        if N<2:return 0
-        if k>=N/2:
-            return sum(max(a-b,0) for a,b in zip(prices[1:],prices[:-1]))
-        globalMax = defaultdict(int)
+#         N = len(prices)
+#         if N<2:return 0
+#         if k>=N/2:
+#             return sum(max(a-b,0) for a,b in zip(prices[1:],prices[:-1]))
+#         globalMax = defaultdict(int)
         
-        for transCount in range(1,k+1):
-            localMax = defaultdict(int)
-            for sellDay in range(1,N):
-                profit = prices[sellDay]-prices[sellDay-1]
-                localMax[sellDay] = max(
-                    globalMax[transCount-1,sellDay-1]+profit,
-                    globalMax[transCount-1,sellDay-1],
-                    localMax[sellDay-1]+profit
-                )
-                
-                globalMax[transCount,sellDay] = max(globalMax[transCount,sellDay-1],localMax[sellDay])
+#         for transCount in range(1,k+1):
+#             localMax = defaultdict(int)
+#             for sellDay in range(1,N):
+#                 profit = prices[sellDay]-prices[sellDay-1]
+#                 localMax[sellDay] = max(
+#                     globalMax[transCount-1,sellDay-1]+profit,
+#                     globalMax[transCount-1,sellDay-1],
+#                     localMax[sellDay-1]+profit
+#                 )
+#                 globalMax[transCount,sellDay] = max(globalMax[transCount,sellDay-1],
+#                                                     localMax[sellDay])
         
-        return globalMax[k,N-1]
+#         return globalMax[k,N-1]
+
+        min_price = [inf] * (k + 1)
+        max_profit = [0] * (k + 1)
+        
+        for price in prices:
+            for i in range(1, k + 1):
+                min_price[i] = min(min_price[i], price - max_profit[i-1])
+                max_profit[i] = max(max_profit[i], price - min_price[i])
+
+        return max_profit[k]

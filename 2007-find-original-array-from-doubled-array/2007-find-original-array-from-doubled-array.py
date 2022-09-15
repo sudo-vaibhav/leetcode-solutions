8be73f2bea670a10e4a116a449902eb1a4e5aa0b
Vaibhav Chopra <53619134+sudo-vaibhav@users.noreply.github.com>
2022-09-15 13:49:04 +0530

    Time: 3664 ms (7.48%), Space: 33.6 MB (20.64%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,14 +1,13 @@
 from sortedcontainers import SortedList
-
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        l = SortedList(changed)
+        sl = sorted(changed)
         ans = []
-        while l:
-            smallest = l[0]
-            l.discard(smallest)
-            if 2*smallest not in l:
-                return []
-            ans.append(smallest)
-            l.discard(2*smallest)
+        items = Counter(changed)
+        for elem in sl:
+            if items[elem]:
+                items[elem]-=1
+                if items[elem*2]==0: return []
+                items[elem*2]-=1
+                ans.append(elem)
         return ans

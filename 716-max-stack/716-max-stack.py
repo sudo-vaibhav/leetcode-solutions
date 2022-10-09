3c1ee0387a93bdab1cd1e4821c7bdfad1097d927
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2022-10-09 06:33:39 +0530

    Time: 2404 ms (5.04%), Space: 59.9 MB (78.90%) - LeetHub

--- 716-max-stack/716-max-stack.py
+++ 716-max-stack/716-max-stack.py
@@ -1,30 +1,38 @@
+from sortedcontainers import SortedList
+
 class MaxStack:
 
     def __init__(self):
-        self.st = []
-        self.maxSt = []
+        self.stack = SortedList() # pushing order
+        self.values = SortedList() # by value
+        self.cnt = 0 # to give unique ids to each element
 
     def push(self, x: int) -> None:
-        self.st.append(x)
-        self.maxSt.append(max(x,(-inf if not self.maxSt else self.maxSt[-1])))
-
+        self.stack.add((self.cnt,x))
+        self.values.add((x,self.cnt))
+        self.cnt += 1
+        
     def pop(self) -> int:
-        self.maxSt.pop()
-        return self.st.pop()
+        idx,val = self.stack.pop()
+        self.values.remove((val,idx))
+        return val
 
     def top(self) -> int:
-        return self.st[-1]
+        return self.stack[-1][1]
 
     def peekMax(self) -> int:
-        return self.maxSt[-1]
+        return self.values[-1][0]
 
     def popMax(self) -> int:
-        temp = []
-        while self.st[-1]!=self.maxSt[-1]:
-            temp.append(self.st.pop())
-            self.maxSt.pop()
-        ans = self.st.pop()
-        self.maxSt.pop()
-        while temp:
-            self.push(temp.pop())
-        return ans
+        val,idx = self.values.pop()
+        self.stack.remove((idx,val))
+        return val
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()

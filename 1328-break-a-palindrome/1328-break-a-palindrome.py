0a49ba77dbe11f048ef211844207036fb4842177
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2022-10-10 05:43:33 +0530

    Time: 941 ms (5.43%), Space: 13.9 MB (13.13%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,15 +1,12 @@
 class Solution:
     def breakPalindrome(self, s: str) -> str:
-        
         if len(s)==1: return ""
         n = len(s)
         lowest = None
-        for i in range(n):
+        for i in range(n): # O(n)
             cur = s[i]
-            for rw in string.ascii_lowercase:
-                if rw!=cur:
-                    temp = s[:i]+rw+s[i+1:]
-                    if temp!=temp[::-1]:
-                        if lowest==None or temp<lowest:
-                            lowest = temp
+            for rw in string.ascii_lowercase:# O(1)
+                temp = s[:i]+rw+s[i+1:]
+                if rw!=cur and temp!=temp[::-1] and (lowest==None or temp<lowest):
+                    lowest = temp
         return lowest

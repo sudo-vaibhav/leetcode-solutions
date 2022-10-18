fb0def9f7bd6de9573d1182bc4c417ce68e4f15f
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2022-10-18 11:23:24 +0530

    Time: 109 ms (22.51%), Space: 13.9 MB (54.87%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1: return "1"
+        base = self.countAndSay(n-1)
+        cur,r,cnt,N,ans = None,0,0,len(base),""
+        while r<N:
+            cur = base[r]
+            while r<N and base[r]==cur:
+                r+=1
+                cnt+=1
+            ans+=str(cnt)+cur
+            cnt=0
+        return ans
+            
+            

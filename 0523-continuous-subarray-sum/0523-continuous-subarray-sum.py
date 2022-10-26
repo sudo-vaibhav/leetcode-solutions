fd4e4369407e89791687508ac099212c8323b607
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2022-10-26 15:28:50 +0530

    Time: 2100 ms (58.02%), Space: 33.8 MB (18.13%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,20 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n = len(nums)
-        if n<2:return False
         seen = defaultdict(lambda:inf)
-        seen[nums[0]%k]=0
-        temp = (nums[0]+nums[1])%k
-        seen[temp]=min(1,seen[temp])
-        running = nums[0]+nums[1]
-        seen[0]=-1
-        if sum(nums[:2])%k==0:return True
+        seen[0] = -1
+        prevRunning,curRunning=0,sum(nums[:2])
+        if n>=2 and (curRunning%k==0): return True 
         for i in range(2,n):
-            running+=nums[i]
-            # print(running%k,seen)
-            if seen[running%k]<i-1:
+            prev = i-2
+            prevRunning+=nums[prev]
+            curRunning+=nums[i]
+            seen[prevRunning%k] = min(seen[prevRunning%k],prev)
+            if seen[curRunning%k]<i-1:
                 return True
-            seen[running%k]=min(seen[running%k],i)
         return False
             
             

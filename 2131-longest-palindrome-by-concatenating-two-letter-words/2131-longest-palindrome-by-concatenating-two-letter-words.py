777059a1ad63ce59c93beb29c5a21f79385f4b3a
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2022-11-03 17:08:27 +0530

    Time: 4615 ms (5.01%), Space: 38.5 MB (12.75%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -2,7 +2,6 @@ class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         
         d = defaultdict(lambda : [0,0])
-        
         for word in words:
             sortedWord = tuple(sorted(word))
             word = tuple(word)
@@ -14,14 +13,12 @@ class Solution:
                 d[sortedWord][0]+=1
             else:
                 d[sortedWord][1]+=1
-        # print(d)
         ans = 0
         for key in d:
             pairs = min(d[key])
             ans += 4*pairs
             for i in range(2):
                 d[key][i]-=pairs
-        
 #         check for any free middle element
         for key in d:
             if key==key[::-1] and d[key][0]>0:

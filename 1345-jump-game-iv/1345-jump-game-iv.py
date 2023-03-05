efc94dad338d7fb17bf184fa36ddbb6bdfc446ec
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-03-05 12:06:58 +0530

    Time: 825 ms (49.59%), Space: 29 MB (60.49%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -1,37 +1,20 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
-        n = len(arr)
-        seen = set()
-        valSeen = set()
-        seen.add(n-1)
-        # valSeen.add(arr[n-1])
-        q = deque()
-        q.append(n-1)
-        pos = defaultdict(list)
-        for idx, i in enumerate(arr):
-            pos[i].append(idx)
-            
-        steps = 0
+        n,pos,steps = len(arr),defaultdict(list),0
+        seen,q = set([n-1]),deque([n-1])
+        for idx, i in enumerate(arr): pos[i].append(idx)
         while q:
-            
             lenQ = len(q)
-            
             for _ in range(lenQ):
-                
                 cur = q.popleft()
-                val = arr[cur]
-                if cur==0:
-                    return steps
-                
+                if cur == 0: return steps
                 for i in filter(
-                    lambda x:0<=x<n,
-                    [cur-1,cur+1,*([] if val in valSeen else pos[val])]
+                    lambda x:0<=x<n and x not in seen,
+                    set([cur-1,cur+1,*pos[arr[cur]]])
                 ):
-                    
-                    if i not in seen:
-                        seen.add(i)
-                        q.append(i)
-                valSeen.add(val)
+                    seen.add(i)
+                    q.append(i)
+                del pos[arr[cur]]
             steps+=1
                      
                 

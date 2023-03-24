21f7ef1f00766819c1f170cf61cbae49959bf800
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-03-24 22:13:53 +0530

    Time: 1222 ms (77.33%), Space: 69.8 MB (53.81%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        gph = defaultdict(list)
+        for u,v in connections:
+            gph[u].append((v,True))
+            gph[v].append((u,False))
+        
+        def solve(node,prev=-1):
+            ans = 0
+            for nex,dn in gph[node]:
+                if nex!=prev:
+                    if dn==True: ans+=1
+                    ans+=solve(nex,node)
+            return ans
+        
+        return solve(0)

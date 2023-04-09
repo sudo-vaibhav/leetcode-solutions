60d6e0e1e4fea3286f21fed61f6a5568e73864ec
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-04-10 00:55:04 +0530

    Time: 431 ms (98.38%), Space: 20.4 MB (18.18%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
@@ -0,0 +1,5 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        
+        score.sort(key=lambda r:r[k],reverse=True)
+        return score

09e24e27519006ac63a3eee24a92ca93162029c7
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-04-17 00:07:41 +0530

    Time: 2550 ms (43.97%), Space: 268.9 MB (26.07%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,21 +1,17 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        m,n = len(words),len(words[0])
-        
+        m,n,MOD = len(words),len(words[0]),10**9+7
         wordsByCol = defaultdict(lambda:defaultdict(int))
+        
         for col in range(n):
             for row in range(m):
                 wordsByCol[col][words[row][col]]+=1
         
-        MOD = 10**9+7
-        
         @cache
         def solve(charIdx, colIdx):
-            if charIdx==len(target):
-                return 1
+            if charIdx==len(target): return 1
             if colIdx==n: return 0
             cur = target[charIdx]
-            ans = (solve(charIdx,colIdx+1)+wordsByCol[colIdx][cur]*solve(charIdx+1,colIdx+1))%MOD
-            return ans
+            return (solve(charIdx,colIdx+1)+wordsByCol[colIdx][cur]*solve(charIdx+1,colIdx+1))%MOD
         
         return solve(0,0)

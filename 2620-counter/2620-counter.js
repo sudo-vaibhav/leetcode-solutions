8d66bbc6658507292c80d443da43bc498247b0e9
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-05-06 13:58:24 +0530

    Time: 58 ms (51.06%), Space: 41.5 MB (90.25%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -3,9 +3,9 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    let c = n;
+    // let c = n;
     return function() {
-        return c++;
+        return n++;
     };
 };
 

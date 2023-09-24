a09fe65d6e7f9d6725cfe16b988a8d4456b2b2bc
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-09-24 06:48:56 +0530

    Time: 266 ms (5.06%), Space: 16.5 MB (27.06%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -0,0 +1,13 @@
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        fullness = defaultdict(lambda : defaultdict(int))
+        
+        fullness[0][0]+=poured
+        
+        for row in range(100):
+            for glass in range(row+1):
+                if fullness[row][glass]>1:
+                    split = (fullness[row][glass]-1)/2
+                    fullness[row+1][glass]+=split
+                    fullness[row+1][glass+1]+=split
+        return min(1,fullness[query_row][query_glass])

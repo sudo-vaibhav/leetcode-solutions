be41a438fa50bed61bf04b68fe3670980925e8aa
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-09-25 23:36:42 +0530

    Time: 38 ms (78.35%), Space: 16.2 MB (76.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,3 +1,3 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        return [x[0] for x in Counter(s+t).items() if x[1]%2][0]
+        return chr(sum(map(ord,t))-sum(map(ord,s)))

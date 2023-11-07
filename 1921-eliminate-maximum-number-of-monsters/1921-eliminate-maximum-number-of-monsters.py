08dadd4263a6130b2ac53f292d4d010a5866f058
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-11-07 16:49:18 +0530

    Time: 681 ms (88.37%), Space: 32.1 MB (62.27%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -1,17 +1,8 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        
-        ttr = [d/s for d,s in zip(dist,speed)]
-        ttr.sort()
+        ttr = sorted([d/s for d,s in zip(dist,speed)])
         ans = 0
-        
-        prevDefeated = -inf
-        # time = 0
         for i in range(len(ttr)):
-            if ttr[i]<=i:
-                break
-            else:
-                ans+=1
-                
-        # print(ttr)
+            if ttr[i]<=i: break
+            else: ans+=1
         return ans

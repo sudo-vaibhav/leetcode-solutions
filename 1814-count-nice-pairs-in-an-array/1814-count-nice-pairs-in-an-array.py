aafc1d3d2bdf93b2b58274b62f1110a55f4f5f47
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-11-21 22:08:41 +0530

    Time: 574 ms (70.54%), Space: 26.9 MB (33.99%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -4,12 +4,9 @@ class Solution:
             return int(str(num)[::-1])
         MOD = 10**9+7
         ans = 0
-        # arr = list(map(lambda x:(x,rev(x)),nums))
-        # x + rev(y) = y + rev(x)
-        # x - rev(x) = y - rev(y)
         occs = defaultdict(int)
-        for j in range(len(nums)):
-            temp = nums[j]-rev(nums[j])#arr[j][1]
+        for num in nums:
+            temp = num-rev(num)#arr[j][1]
             ans += occs[temp]
             if ans>=MOD:
                 ans-=MOD

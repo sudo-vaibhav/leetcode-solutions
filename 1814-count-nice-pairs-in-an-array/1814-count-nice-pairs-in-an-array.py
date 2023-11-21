3cb7d219ffcd28d55c3d73ee7a1346509483345f
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2023-11-21 22:11:44 +0530

    Time: 677 ms (16.71%), Space: 30.1 MB (6.80%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -2,7 +2,13 @@ class Solution:
     def countNicePairs(self, nums: List[int]) -> int:
         @cache
         def rev(num:int):
-            return int(str(num)[::-1])
+            temp = 0
+            while num>0:
+                temp *= 10
+                temp += num%10
+                num //= 10
+            return temp
+            # return int(str(num)[::-1])
         MOD,ans,occs = 10**9+7,0,defaultdict(int)
         # ans = 0
         # occs = defaultdict(int)

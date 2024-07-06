f4a33834bbf918402614efdcfc45067ec8be24e2
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2024-07-06 12:07:34 +0530

    Time: 39 ms (29.62%), Space: 16.4 MB (77.72%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,12 +1,6 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
-        p = n-1
-        
-        v = time//p
-        left = time-v*p
-        if v%2==0:
-            # will be from left end
-            return 1+left
-        else:
-            return n-left
+        v = time//(n-1)
+        left = time-v*(n-1)
+        return 1+left if v%2==0 else n-left
             

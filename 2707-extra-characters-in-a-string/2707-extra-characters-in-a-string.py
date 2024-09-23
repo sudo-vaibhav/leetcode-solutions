d5eff0af1ef9a83d7f1b7ec60257134cc2a43c2e
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2024-09-23 13:17:06 +0530

    Time: 209 ms (47.99%), Space: 17.1 MB (46.06%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,17 +1,36 @@
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        n=len(s)
-        dictionary = set(dictionary)
+#         n=len(s)
+#         dictionary = set(dictionary)
+#         @cache
+#         def solve(i):
+#             if i==n:
+#                 return 0
+#             ans = 1+solve(i+1)
+#             temp = ""
+#             for j in range(i,n):
+#                 temp+=s[j]
+#                 if temp in dictionary:
+#                     ans = min(ans,solve(j+1))
+#             return ans
+        
+#         return solve(0)
+
+
+        n = len(s)
+        words = set(dictionary)
         @cache
-        def solve(i):
+        def maxCoverage(i):
             if i==n:
                 return 0
-            ans = 1+solve(i+1)
-            temp = ""
-            for j in range(i,n):
-                temp+=s[j]
-                if temp in dictionary:
-                    ans = min(ans,solve(j+1))
+            running = ""
+            ans = maxCoverage(i+1)
+            for end in range(i,len(s)):
+                running += s[end]
+                if running in words:
+                    ans = max(ans,len(running)+maxCoverage(end+1))
             return ans
-        
-        return solve(0)
+        return n-maxCoverage(0)
+    
+    
+    

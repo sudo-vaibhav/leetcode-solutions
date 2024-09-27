12184efae594715bb5f12854d3190f3becf01011
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2024-09-28 01:22:31 +0530

    Time: 1607 ms (25.30%), Space: 18 MB (16.34%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,26 +1,35 @@
+from sortedcontainers import SortedDict
+
+
 class MyCalendarTwo:
 
     def __init__(self):
-        self.intvls = []
-        self.ovl = []
-    def overlap(self,s1,e1,s2,e2):
-        s = max(s1,s2)
-        e = min(e1,e2)
-        return (s,e) if s<e else False
+        # Store the number of bookings at each point.
+        self.booking_count = SortedDict()
+        # The maximum number of overlapped bookings allowed.
+        self.max_overlapped_booking = 2
+
     def book(self, start: int, end: int) -> bool:
-        # print(start,end,self.ovl)
-        for s,e in self.ovl:
-            if self.overlap(start,end,s,e)!=False:
+        # Increase and decrease the booking count at the start and end respectively.
+        self.booking_count[start] = self.booking_count.get(start, 0) + 1
+        self.booking_count[end] = self.booking_count.get(end, 0) - 1
+
+        overlapped_booking = 0
+
+        # Calculate the prefix sum of bookings.
+        for count in self.booking_count.values():
+            overlapped_booking += count
+            # If the number of overlaps exceeds the allowed limit
+            # rollback and return False.
+            if overlapped_booking > self.max_overlapped_booking:
+                # Rollback changes.
+                self.booking_count[start] -= 1
+                self.booking_count[end] += 1
+
+                # Remove entries if their count becomes zero to clean up the SortedDict.
+                # if self.booking_count[start] == 0:
+                #     del self.booking_count[start]
+
                 return False
-        # print("proceeding")
-        for s,e in self.intvls:
-            temp = self.overlap(start,end,s,e)
-            if temp!=False:
-                self.ovl.append(temp)
-        self.intvls.append((start,end))
+
         return True
-
-
-# Your MyCalendarTwo object will be instantiated and called as such:
-# obj = MyCalendarTwo()
-# param_1 = obj.book(start,end)

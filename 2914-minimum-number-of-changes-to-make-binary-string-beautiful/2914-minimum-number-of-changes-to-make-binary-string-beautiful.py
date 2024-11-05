d2dee3b9b41462af029c003ae002cb7a56b8e7e8
Vaibhav Chopra <mailvaibhavchopra@gmail.com>
2024-11-05 16:07:00 +0530

    Time: 27 ms (29.73%), Space: 16.9 MB (89.02%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,17 +1,9 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        
-        
-        l,r = 0,len(s)
         ans = 0
-        chunks = []
+        # chunks = []
         for i in range(0,len(s),2):
             chunk = s[i:i+2]
-            if Counter(chunk)["1"]%2==1:
+            if chunk in "010":
                 ans+=1
-            # chunks.append()
         return ans
-#         while l<=r:
-#             m = (l+r)//2
-            
-#         return ans
